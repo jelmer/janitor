dc34c44eb3fcc1566f29f21922b203754aaabbcc
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-12-23 01:13:48 +0000

    Fetch from public location.

--- update.sh
+++ update.sh
@@ -3,15 +3,16 @@
 if [ ! -d .plugins ]; then
     mkdir .plugins
     brz branch lp:brz-propose .plugins/propose
+else
+    brz pull -d .plugins/propose
 fi
-brz pull -d ~/src/brz-propose/trunk https://code.breezy-vcs.org/brz-propose/trunk --overwrite
 if [ ! -d lintian-brush ]; then
     brz branch https://salsa.debian.org/jelmer/lintian-brush
 else
     brz pull -d lintian-brush
 fi
 if [ ! -d silver-platter ]; then
-    brz branch bzr+ssh://rhonwyn/srv/bzr/silver-platter/trunk silver-platter
+    brz branch https://jelmer.uk/bzr/silver-platter/trunk silver-platter
 else
     brz pull -d silver-platter
 fi
