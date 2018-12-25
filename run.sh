5b58bc495c2973e397b6b9dba2b6ba584fef1e1a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-12-25 12:32:05 +0000

    Add run commands.

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+BRZ_PLUGINS_AT=propose@$(pwd)/.plugins/propose PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush" ./silver-platter/propose-lintian-fixes.py --pre-check "test ! -f debian/control.in" policy.conf "$@"
