324891d39d5991dcd2aeb8dfeaba2a9e94a372d1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-12-29 17:41:08 +0000

    Add trivial status page.

---
+++ report-state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+con = sqlite3.connect('state.db')
+
+print("Merge Proposals")
+print("===============")
+
+cur = con.cursor()
+cur.execute(
+    "SELECT branch.url, merge_proposal.url FROM branch "
+    "LEFT JOIN merge_proposal ON branch.id = merge_proposal.branch_id")
+for (branch_url, mp_url) in cur.fetchall():
+    print("* %s" % branch_url)
+    if mp_url:
+        print(" - %s" % mp_url)
+    print("")
+
+print("Recent Runs")
+print("===========")
+cur.execute(
+    "SELECT run.command, run.finish_time, merge_proposal.url FROM run "
+    "LEFT JOIN merge_proposal ON run.merge_proposal_id = merge_proposal.id")
+
+for (command, finish_time, merge_proposal_url) in cur.fetchall():
+    print("* %s %s %s" % (command, finish_time, merge_proposal_url))
