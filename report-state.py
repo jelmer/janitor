94eff35e41bd3dafc18367693696416e3230ffd7
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-01-02 22:13:34 +0000

    Avoid using state db.

--- report-state.py
+++ report-state.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
 
-import sqlite3
-import sys
+from breezy import urlutils
+from breezy.plugins.propose.propose import hosters
 
-con = sqlite3.connect(sys.argv[1])
+print("Open Merge Proposals")
+print("====================")
 
-print("Merge Proposals")
-print("===============")
-
-cur = con.cursor()
-cur.execute(
-    "SELECT branch.url, merge_proposal.url FROM branch "
-    "LEFT JOIN merge_proposal ON branch.id = merge_proposal.branch_id")
-for (branch_url, mp_url) in cur.fetchall():
-    print("* %s" % branch_url)
-    if mp_url:
-        print(" - %s" % mp_url)
-    print("")
-
-print("Recent Runs")
-print("===========")
-cur.execute(
-    "SELECT run.command, run.finish_time, merge_proposal.url FROM run "
-    "LEFT JOIN merge_proposal ON run.merge_proposal_id = merge_proposal.id")
-
-for (command, finish_time, merge_proposal_url) in cur.fetchall():
-    print("* %s %s %s" % (command, finish_time, merge_proposal_url))
+for name, hoster_cls in hosters.items():
+    for instance in hoster_cls.iter_instances():
+        for mp in instance.iter_my_proposals():
+            print('- %s' % mp.url)
