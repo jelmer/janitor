578142ee585d84b772a99ec78754a9bff9fd8322
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-01-03 00:17:55 +0000

    Add open/closed/merged merge proposals in status.

--- report-state.py
+++ report-state.py
@@ -3,10 +3,45 @@
 from breezy import urlutils
 from breezy.plugins.propose.propose import hosters
 
-print("Open Merge Proposals")
-print("====================")
+print("""\
+Open Proposals
+==============
+
+These proposals are currently waiting for review.
+""")
 
 for name, hoster_cls in hosters.items():
     for instance in hoster_cls.iter_instances():
-        for mp in instance.iter_my_proposals():
+        for mp in instance.iter_my_proposals(status='open'):
+            print('- %s' % mp.url)
+
+
+print("""
+
+Merged Proposals
+================
+
+These proposals have been merged in the past.
+""")
+
+for name, hoster_cls in hosters.items():
+    for instance in hoster_cls.iter_instances():
+        for mp in instance.iter_my_proposals(status='merged'):
+            print('- %s' % mp.url)
+
+
+print("""
+
+Closed Proposals
+================
+
+Proposals can be closed without being merged for a number of reasons - a
+similar change has already been applied, the change was rejected or the change
+was merged without history being referenced (i.e. in the case of a
+cherry-pick.
+""")
+
+for name, hoster_cls in hosters.items():
+    for instance in hoster_cls.iter_instances():
+        for mp in instance.iter_my_proposals(status='merged'):
             print('- %s' % mp.url)
