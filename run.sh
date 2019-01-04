5e2885a01640ab1b4d16dad6641466ac08e5b09a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-01-04 21:52:44 +0000

    Make build-verify a command.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-BRZ_PLUGINS_AT=propose@$(pwd)/.plugins/propose PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter" ./propose-lintian-fixes.py --pre-check "test ! -f debian/control.in" --policy=policy.conf --build-verify "$@"
+BRZ_PLUGINS_AT=propose@$(pwd)/.plugins/propose PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter" ./propose-lintian-fixes.py --pre-check "test ! -f debian/control.in" --policy=policy.conf "$@"
