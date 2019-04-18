7f11129a6c5ce55c68d43b249c9160b459d3a794
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-18 00:57:01 +0000

    Use directory paths for builds.

--- report-pkg.py
+++ report-pkg.py
@@ -52,12 +52,16 @@ Packages
             f.write('-----------\n')
 
             for run_id, (start_time, finish_time), command, description, package_name, merge_proposal_url in state.iter_runs(name):
-                f.write('* `%s <%s.html>`_' % (command, run_id))
+                f.write('* `%s <%s/>`_' % (command, run_id))
                 if merge_proposal_url:
                     f.write(' (`merge proposal <%s>`_)' % merge_proposal_url)
                 f.write('\n')
 
-                with open(os.path.join(pkg_dir, run_id + '.rst'), 'w') as g:
+                run_dir = os.path.join(pkg_dir, run_id)
+                if not os.path.isdir(run_dir):
+                    os.mkdir(run_dir)
+
+                with open(os.path.join(run_dir, 'index.rst'), 'w') as g:
                     g.write('Run %s\n' % run_id)
                     g.write('====' + len(run_id) * '=' + '\n')
 
@@ -68,10 +72,11 @@ Packages
                     g.write('\n')
                     g.write('%s\n' % description)
                     g.write('\n')
-                    g.write('.. literalinclude:: %s/build.log\n' % run_id)
+                    g.write('.. literalinclude:: ../logs/%s/build.log\n' % run_id)
                     g.write('   :linenos:\n')
                     g.write('   :language: shell\n')
                     g.write("\n")
                     g.write("*Last Updated: " + time.asctime() + "*\n")
+
     indexf.write("\n")
     indexf.write("*Last Updated: " + time.asctime() + "*\n")
