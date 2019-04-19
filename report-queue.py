2365362c5cbb1b084e6de5e4283cd338793c247f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-19 23:41:52 +0000

    Add queue report command.

---
+++ report-queue.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Copyright (C) 2019 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import breezy  # noqa: E402
+breezy.initialize()
+
+from breezy.trace import note
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from janitor import state  # noqa: E402
+
+print("Queue")
+print("=====")
+print("")
+
+for i, (branch_url, mode, env, command) in enumerate(state.iter_queue(pop=False), 1):
+    print("%d. `%s <pkg/%s>`_ (%s)" % (i, env["PACKAGE"], env["PACKAGE"], command[0]))
