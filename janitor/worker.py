ac7e51bff0c487a4669ae28791c7a746368b7519
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-20 23:26:09 +0000

    Convert over to Workspace.

--- janitor/worker.py
+++ janitor/worker.py
@@ -20,8 +20,6 @@ from datetime import datetime
 import distro_info
 import os
 import socket
-import subprocess
-import uuid
 
 from breezy.branch import Branch
 from breezy import (
@@ -31,28 +29,26 @@ from breezy.trace import (
     note,
     warning,
 )
-from breezy.plugins.propose.propose import (
-    NoSuchProject,
-    UnsupportedHoster,
-)
-
 
 from silver_platter.debian import (
-    propose_or_push,
     BuildFailedError,
     MissingUpstreamTarball,
+    Workspace,
 )
 from silver_platter.debian.lintian import (
     available_lintian_fixers,
-    PostCheckFailed,
-    LintianFixer,
-    create_mp_description,
     get_fixers,
-    parse_mp_description,
+    LintianFixer,
     DEFAULT_ADDON_FIXERS,
 )
 from silver_platter.debian.upstream import (
     NewUpstreamMerger,
+)
+
+from silver_platter.utils import (
+    run_pre_check,
+    run_post_check,
+    PostCheckFailed,
 )
 
 from janitor.build import (
@@ -61,35 +57,10 @@ from janitor.build import (
     get_latest_changelog_version,
     changes_filename,
     get_build_architecture,
-)  # noqa: E402
+)
 
 
-JANITOR_BLURB = """
-This merge proposal was created automatically by the Janitor bot
-(https://janitor.debian.net/).
-
-You can follow up to this merge proposal as you normally would.
-"""
-
-
-LOG_BLURB = """
-Build and test logs for this branch can be found at
-https://janitor.debian.net/pkg/%(package)s/%(log_id)s/.
-"""
-
-
-def strip_janitor_blurb(text):
-    return text[text.index(JANITOR_BLURB):]
-
-
-def add_janitor_blurb(text, env):
-    text += JANITOR_BLURB
-    if env['log_id']:
-        text += (LOG_BLURB % env)
-    return text
-
-
-class JanitorLintianFixer(LintianFixer):
+class JanitorLintianFixer(object):
     """Janitor-specific Lintian Fixer."""
 
     @staticmethod
@@ -109,46 +80,8 @@ class JanitorLintianFixer(LintianFixer):
             help='Fixers that should be considered add-on-only.',
             type=str, action='append', default=DEFAULT_ADDON_FIXERS)
 
-    def __init__(self, pkg, fixers, update_changelog, compat_release,
-                 pre_check=None, post_check=None, propose_addon_only=None,
-                 committer=None, log_id=None):
-        super(JanitorLintianFixer, self).__init__(
-            pkg, fixers=fixers, update_changelog=update_changelog,
-            compat_release=compat_release, pre_check=pre_check,
-            post_check=post_check, propose_addon_only=propose_addon_only,
-            committer=committer)
-        self._log_id = log_id
 
-    def get_proposal_description(self, existing_proposal):
-        if existing_proposal:
-            existing_description = existing_proposal.get_description()
-            existing_description = strip_janitor_blurb(existing_description)
-            existing_lines = parse_mp_description(existing_description)
-        else:
-            existing_lines = []
-        return add_janitor_blurb(create_mp_description(
-            existing_lines + [l for r, l in self.applied]), {
-                'package': self._pkg, 'log_id': self._log_id})
-
-    def describe(self, result):
-        tags = set()
-        for brush_result, unused_summary in self.applied:
-            tags.update(brush_result.fixed_lintian_tags)
-        if result.merge_proposal:
-            if result.is_new:
-                return 'Proposed fixes %r' % tags
-            elif tags:
-                return 'Updated proposal with fixes %r' % tags
-            else:
-                return 'No new fixes for proposal'
-        else:
-            if tags:
-                return 'Pushed fixes %r' % tags
-            else:
-                return 'Nothing to do.'
-
-
-class JanitorNewUpstreamMerger(NewUpstreamMerger):
+class JanitorNewUpstreamMerger(object):
 
     @staticmethod
     def setup_argparser(subparser):
@@ -157,35 +90,19 @@ class JanitorNewUpstreamMerger(NewUpstreamMerger):
             help='Merge a new upstream snapshot rather than a release',
             action='store_true')
 
-    def describe(self, result):
-        if result.merge_proposal:
-            if result.is_new:
-                return (
-                    'Created merge proposal %s merging new '
-                    'upstream version %s.' % (
-                        result.merge_proposal.url,
-                        self._upstream_version))
-            else:
-                return 'Updated merge proposal %s for upstream version %s.' % (
-                    result.merge_proposal.url, self._upstream_version)
-        return 'Did nothing.'
-
 
 class WorkerResult(object):
 
-    def __init__(self, pkg, log_id, start_time, finish_time, description,
-                 proposal_url=None, is_new=None, build_distribution=None,
+    def __init__(self, description, build_distribution=None,
                  build_version=None, changes_filename=None):
-        self.package = pkg
-        self.log_id = log_id
-        self.start_time = start_time
-        self.finish_time = finish_time
         self.description = description
-        self.proposal_url = proposal_url
-        self.is_new = is_new
         self.build_version = build_version
         self.build_distribution = build_distribution
         self.changes_filename = changes_filename
+
+
+class WorkerFailure(Exception):
+    """Worker processing failed."""
 
 
 debian_info = distro_info.DebianDistroInfo()
@@ -198,11 +115,10 @@ new_upstream_subparser = argparse.ArgumentParser(prog='new-upstream')
 JanitorNewUpstreamMerger.setup_argparser(new_upstream_subparser)
 
 
-def process_package(vcs_url, mode, env, command, output_directory,
-                    dry_run=False, refresh=False,
+def process_package(vcs_url, env, command, output_directory,
                     build_command=None, pre_check_command=None,
                     post_check_command=None, possible_transports=None,
-                    possible_hosters=None):
+                    possible_hosters=None, resume_branch_url=None):
     pkg = env['PACKAGE']
     committer = env['COMMITTER']
     # TODO(jelmer): sort out this mess:
@@ -215,51 +131,81 @@ def process_package(vcs_url, mode, env, command, output_directory,
         build_version_suffix = 'janitor+newupstream'
         build_suite = 'upstream-releases'
     else:
-        raise AssertionError('unknown subcommand %s' % command[0])
-    log_id = str(uuid.uuid4())
+        raise WorkerFailure('unknown subcommand %s' % command[0])
     assert pkg is not None
     assert output_directory is not None
     output_directory = os.path.abspath(output_directory)
-    log_path = os.path.join(output_directory, pkg, 'logs', log_id)
-    os.makedirs(log_path)
 
-    build_details = []
+    note('Processing: %s', pkg)
 
-    if pre_check_command:
-        def pre_check(local_tree):
-            try:
-                subprocess.check_call(
-                    pre_check_command, shell=True, cwd=local_tree.basedir)
-            except subprocess.CalledProcessError:
-                note('%r: pre-check failed, skipping', pkg)
-                return False
-            return True
-    else:
-        pre_check = None
+    try:
+        main_branch = Branch.open(
+            vcs_url, possible_transports=possible_transports)
+        if resume_branch_url:
+            resume_branch = Branch.open(
+                resume_branch_url,
+                possible_transports=possible_transports)
+        else:
+            resume_branch = None
+    except socket.error:
+        raise WorkerFailure('ignoring, socket error')
+    except errors.NotBranchError as e:
+        raise WorkerFailure('Branch does not exist: %s' % e)
+    except errors.UnsupportedProtocol:
+        raise WorkerFailure('Branch available over unsupported protocol')
+    except errors.ConnectionError as e:
+        raise WorkerFailure(str(e))
+    except errors.PermissionDenied as e:
+        raise WorkerFailure(str(e))
+    except errors.InvalidHttpResponse as e:
+        raise WorkerFailure(str(e))
+    except errors.TransportError as e:
+        raise WorkerFailure(str(e))
 
-    def post_check(local_tree, since_revid):
-        if post_check_command:
-            try:
-                subprocess.check_call(
-                    post_check_command, shell=True, cwd=local_tree.basedir,
-                    env={'SINCE_REVID': since_revid})
-            except subprocess.CalledProcessError:
-                note('%s: post-check failed, skipping', pkg)
-                return False
+    with Workspace(main_branch, resume_branch=resume_branch) as ws:
+        run_pre_check(ws.local_tree, pre_check_command)
+
+        if command[0] == 'lintian-brush':
+            # TODO(jelmer): 'fixers' is wrong; it's actually tags.
+            fixers = get_fixers(
+                available_lintian_fixers(), tags=subargs.fixers)
+            branch_changer = LintianFixer(
+                pkg, fixers=fixers,
+                update_changelog=subargs.update_changelog,
+                compat_release=subargs.compat_release,
+                propose_addon_only=subargs.propose_addon_only,
+                committer=committer)
+        elif command[0] == 'new-upstream':
+            branch_changer = NewUpstreamMerger(
+                subargs.snapshot)
+
+        branch_changer.make_changes(ws.local_tree)
+
+        if not ws.changes_since_main():
+            raise WorkerResult('Nothing to do.')
+
+        try:
+            run_post_check(ws.local_tree, post_check_command, ws.orig_revid)
+        except PostCheckFailed as e:
+            note('%s: post-check failed')
+            raise WorkerFailure(str(e))
+
         if build_command:
             add_dummy_changelog_entry(
-                local_tree.basedir, build_version_suffix, build_suite,
+                ws.local_tree.basedir, build_version_suffix, build_suite,
                 'Build for debian-janitor apt repository.')
-            with open(os.path.join(log_path, 'build.log'), 'w') as f:
+            with open(os.path.join(output_directory, 'build.log'), 'w') as f:
                 try:
-                    build(local_tree, outf=f, build_command=build_command,
+                    build(ws.local_tree, outf=f, build_command=build_command,
                           result_dir=output_directory,
                           distribution=build_suite)
                 except BuildFailedError:
-                    note('%s: build failed, skipping', pkg)
-                    return False
+                    raise WorkerFailure('build failed')
+                except MissingUpstreamTarball:
+                    raise WorkerFailure('unable to find upstream source')
+
             (cl_package, cl_version) = get_latest_changelog_version(
-                local_tree)
+                ws.local_tree)
             changes_name = changes_filename(
                 cl_package, cl_version, get_build_architecture())
             changes_path = os.path.join(
@@ -267,93 +213,21 @@ def process_package(vcs_url, mode, env, command, output_directory,
             if not os.path.exists(changes_path):
                 warning('Expected changes path %s does not exist.',
                         changes_path)
+                build_suite = None
+                changes_name = None
+                cl_version = None
             else:
-                build_details.append(
-                    (cl_version, build_suite, changes_name))
-        return True
-
-    note('Processing: %s (mode: %s)', pkg, mode)
-    start_time = datetime.now()
-
-    try:
-        main_branch = Branch.open(
-            vcs_url, possible_transports=possible_transports)
-    except socket.error:
-        return WorkerResult(
-            pkg, log_id, start_time, datetime.now(), 'ignoring, socket error')
-    except errors.NotBranchError as e:
-        return WorkerResult(
-            pkg, log_id, start_time, datetime.now(),
-            'Branch does not exist: %s' % e)
-    except errors.UnsupportedProtocol:
-        return WorkerResult(
-            pkg, log_id, start_time, datetime.now(),
-            'Branch available over unsupported protocol')
-    except errors.ConnectionError as e:
-        return WorkerResult(pkg, log_id, start_time, datetime.now(), str(e))
-    except errors.PermissionDenied as e:
-        return WorkerResult(pkg, log_id, start_time, datetime.now(), str(e))
-    except errors.InvalidHttpResponse as e:
-        return WorkerResult(pkg, log_id, start_time, datetime.now(), str(e))
-    except errors.TransportError as e:
-        return WorkerResult(pkg, log_id, start_time, datetime.now(), str(e))
-    else:
-        if command[0] == 'lintian-brush':
-            # TODO(jelmer): 'fixers' is wrong; it's actually tags.
-            fixers = get_fixers(
-                available_lintian_fixers(), tags=subargs.fixers)
-            branch_changer = JanitorLintianFixer(
-                pkg, fixers=fixers,
-                update_changelog=subargs.update_changelog,
-                compat_release=subargs.compat_release,
-                pre_check=pre_check, post_check=post_check,
-                propose_addon_only=subargs.propose_addon_only,
-                committer=committer, log_id=log_id)
-            branch_name = "lintian-fixes"
-        elif command[0] == 'new-upstream':
-            branch_changer = JanitorNewUpstreamMerger(
-                subargs.snapshot, post_check=post_check,
-                pre_check=pre_check)
-            branch_name = "new-upstream"
-        if mode == 'build-only':
-            dry_run = True
-            mode = 'propose'
-        try:
-            result = propose_or_push(
-                main_branch, branch_name, branch_changer, mode,
-                possible_transports=possible_transports,
-                possible_hosters=possible_hosters,
-                refresh=refresh, dry_run=dry_run)
-        except UnsupportedHoster:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(), 'Hosted unsupported.')
-        except NoSuchProject as e:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(),
-                'project %s was not found' % e.project)
-        except BuildFailedError:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(), 'build failed')
-        except MissingUpstreamTarball:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(),
-                'unable to find upstream source')
-        except errors.PermissionDenied as e:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(), str(e))
-        except PostCheckFailed as e:
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(), str(e))
+                note('Built %s', changes_path)
         else:
-            description = branch_changer.describe(result)
-            return WorkerResult(
-                pkg, log_id, start_time, datetime.now(),
-                description, build_distribution=(
-                    build_details[0][1] if build_details else None),
-                build_version=(
-                    build_details[0][0] if build_details else None),
-                changes_filename=(
-                    build_details[0][2] if build_details else None))
+            build_suite = None
+            changes_name = None
+            cl_version = None
+
+        return WorkerResult(
+            'Success',
+            build_distribution=build_suite,
+            changes_filename=changes_name,
+            build_version=cl_version)
 
 
 def main(argv=None):
@@ -367,13 +241,8 @@ def main(argv=None):
         '--branch-url', type=str,
         help='URL of branch to build.')
     parser.add_argument(
-        "--dry-run",
-        help="Create branches but don't push or propose anything.",
-        action="store_true", default=False)
-    parser.add_argument(
-        '--refresh', action="store_true",
-        help='Refresh branch (discard current branch) and '
-        'create from scratch')
+        '--resume-branch-url', type=str,
+        help='URL of resume branch to continue on (if any).')
     parser.add_argument(
         '--pre-check',
         help='Command to run to check whether to process package.',
@@ -386,11 +255,6 @@ def main(argv=None):
         '--build-command',
         help='Build package to verify it.', type=str,
         default='sbuild -v')
-    parser.add_argument(
-        '--mode',
-        help='Mode for pushing',
-        choices=['push', 'attempt-push', 'propose', 'build-only'],
-        default="propose", type=str)
 
     parser.add_argument('command', nargs=argparse.REMAINDER)
 
@@ -398,17 +262,24 @@ def main(argv=None):
     if args.branch_url is None:
         parser.print_usage()
         return 1
-    result = process_package(
-        args.branch_url, args.mode, os.environ,
-        args.command, output_directory=args.output_directory,
-        dry_run=args.dry_run, refresh=args.refresh,
-        build_command=args.build_command, pre_check_command=args.pre_check,
-        post_check_command=args.post_check)
-    if result.proposal_url:
-        note('%s: %s: %s', result.package, result.description,
-             result.proposal_url)
+    start_time = datetime.now()
+    try:
+        result = process_package(
+            args.branch_url, os.environ,
+            args.command, output_directory=args.output_directory,
+            build_command=args.build_command, pre_check_command=args.pre_check,
+            post_check_command=args.post_check,
+            resume_branch_url=args.resume_branch_url)
+    except WorkerFailure as e:
+        note('Elapsed time: %s', datetime.now() - start_time)
+        note('Worker failed: %s', e)
+        return 1
     else:
-        note('%s: %s', result.package, result.description)
+        note('Elapsed time: %s', datetime.now() - start_time)
+        note('%s', result.description)
+        if result.changes_filename is not None:
+            note('Built %s.', result.changes_filename)
+        return 0
 
 
 if __name__ == '__main__':
