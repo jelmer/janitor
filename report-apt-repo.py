323ed63bb8f4fb6362f7e3391c65a426d5773394
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-22 12:25:19 +0000

    just report upstream version, improve some wording.

--- report-apt-repo.py
+++ report-apt-repo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+from debian.changelog import Version
 import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
@@ -11,6 +12,5 @@ parser = argparse.ArgumentParser(prog='report-apt-repo')
 parser.add_argument("suite")
 args = parser.parse_args()
 
-for source, version in state.iter_published_packages(
-        args.suite):
-    sys.stdout.write('* %s %s\n' % (source, version))
+for source, version in state.iter_published_packages(args.suite):
+    sys.stdout.write('* %s %s\n' % (source, Version(version).upstream_version))
