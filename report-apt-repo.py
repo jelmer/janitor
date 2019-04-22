6f2c5a0bba4b6c98b2091dc9421891eb5f502b73
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-22 15:34:18 +0000

    Report upstream versions.

--- report-apt-repo.py
+++ report-apt-repo.py
@@ -6,7 +6,7 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
-from janitor import state  # noqa: E402
+from janitor import state, udd  # noqa: E402
 
 parser = argparse.ArgumentParser(prog='report-apt-repo')
 parser.add_argument("suite")
@@ -15,17 +15,19 @@ args = parser.parse_args()
 present = {}
 
 for source, version in state.iter_published_packages(args.suite):
-    present[source] = version
+    present[source] = Version(version)
 
 unstable = {}
-for package in state.get_source_packages(
-        packages=set(present), release='sid'):
-    unstable[package.name] = package.version
+if present:
+    for package in udd.UDD.public_udd_mirror().get_source_packages(
+            packages=list(present), release='sid'):
+        unstable[package.name] = Version(package.version)
 
 for source in sorted(present):
     sys.stdout.write(
         '* %s %s' %
-        source, Version(present[source]).upstream_version)
+        (source, present[source].upstream_version))
     if source in unstable:
-        sys.stdout.write(' (%s in unstable)' % (unstable[source], ))
+        sys.stdout.write(' (%s in unstable)' % (
+            unstable[source].upstream_version, ))
     sys.stdout.write('\n')
