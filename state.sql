14641a062fc5cf475943cd51c6b172ac9d8023fe
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-24 00:46:00 +0000

    Mostly migrate to postgresql.

--- state.sql
+++ state.sql
@@ -1,39 +1,41 @@
 CREATE TABLE IF NOT EXISTS package (
-   name string not null,
-   branch_url string not null,
-   maintainer_email string not null,
+   name text not null,
+   branch_url text not null,
+   maintainer_email text not null,
    primary key(name), unique(branch_url)
 );
+CREATE TYPE merge_proposal_status AS ENUM ('open', 'closed', 'merged');
 CREATE TABLE IF NOT EXISTS merge_proposal (
-   package string,
-   url string not null,
-   status string check(status in ("open", "closed", "merged")) NULL DEFAULT NULL,
+   package text,
+   url text not null,
+   status merge_proposal_status NULL DEFAULT NULL,
    foreign key (package) references package(name),
    primary key(url)
 );
 CREATE TABLE IF NOT EXISTS run (
-   id string not null primary key,
-   command string,
-   description string,
-   start_time string,
-   finish_time string,
-   package string not null,
+   id text not null primary key,
+   command text,
+   description text,
+   start_time timestamp,
+   finish_time timestamp,
+   package text not null,
    -- Associated merge proposal URL, if any.
-   merge_proposal_url string null,
-   -- Debian version string of the built package
-   build_version string,
+   merge_proposal_url text null,
+   -- Debian version text of the built package
+   build_version text,
    -- Distribution the package was built for (e.g. "lintian-fixes")
-   build_distribution string,
-   result_code string,
+   build_distribution text,
+   result_code text,
    foreign key (package) references package(name),
    foreign key (merge_proposal_url) references merge_proposal(url)
 );
+CREATE TYPE publish_mode AS ENUM('push', 'attempt-push', 'propose', 'build-only');
 CREATE TABLE IF NOT EXISTS queue (
-   id integer primary key autoincrement,
-   package string not null,
-   command string not null,
-   committer string null,
-   mode string check(mode in ("push", "attempt-push", "propose", "build-only")) not null,
+   id serial,
+   package text not null,
+   command text not null,
+   committer text null,
+   mode publish_mode not null,
    priority integer default 0,
    foreign key (package) references package(name),
    unique(package, command, mode)
