1866efcd9004d9c50b2fb83c4e5f07f53889e44a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-24 01:50:34 +0000

    Avoid repeated db queries.

--- report-pkg.py
+++ report-pkg.py
@@ -44,6 +44,91 @@ def include_console_log(f, log_path, tail=None):
 if not os.path.isdir(dir):
     os.mkdir(dir)
 
+
+runs_by_pkg = {}
+
+for run in state.iter_runs():
+    (run_id, (start_time, finish_time), command, description,
+        package_name, merge_proposal_url, build_version,
+        build_distro, result_code) = run
+
+    runs_by_pkg.setdefault(package_name, []).append(run)
+
+    run_dir = os.path.join(dir, package_name, run_id)
+    os.makedirs(run_dir, exist_ok=True)
+
+    kind = command.split(' ')[0]
+    with open(os.path.join(run_dir, 'index.rst'), 'w') as g:
+        g.write('Run of %s for %s\n' % (kind, package_name))
+        g.write('====' + len(run_id) * '=' + '\n')
+
+        g.write('* Package: `%s <..>`_\n' % package_name)
+        g.write('* Start time: %s\n' % start_time)
+        g.write('* Finish time: %s\n' % finish_time)
+        g.write('* Run time: %s\n' % (finish_time - start_time))
+        g.write('* Description: %s\n' % description)
+        if build_version:
+            changes_name = changes_filename(
+                package_name, build_version,
+                get_build_architecture())
+            g.write('* Changes filename: `%s '
+                    '<../../../%s/%s>`_\n'
+                    % (changes_name, build_distro, changes_name))
+        g.write('\n')
+        g.write('Try this locally::\n\n\t')
+        # TODO(jelmer): Don't put lintian-fixer specific code here
+        svp_args = command.split(' ')
+        if svp_args[0] == 'lintian-brush':
+            g.write('debian-svp lintian-brush %s %s' % (
+                package_name, ' '.join(
+                    ['--fixers=%s' % f for f in svp_args[1:]])))
+        elif svp_args[0] == 'new-upstream':
+
+            g.write('debian-svp new-upstream %s' % package_name)
+            if svp_args[1:]:
+                g.write(' ' + ' '.join(svp_args[1:]))
+        else:
+            raise AssertionError
+        g.write('\n\n')
+        build_log_path = 'build.log'
+        worker_log_path = 'worker.log'
+        if build_version:
+            changes_name = changes_filename(
+                package_name, build_version,
+                get_build_architecture())
+            changes_path = os.path.join(
+                "../public_html", build_distro, changes_name)
+            if not os.path.exists(changes_path):
+                warning('Missing changes path %r', changes_path)
+            else:
+                g.write('Install this package (if you have the ')
+                g.write('`apt repository <../../../>`_ enabled) '
+                        'by running one of::\n\n')
+                for binary in changes_get_binaries(changes_path):
+                    g.write(
+                        '\tapt install -t upstream-releases %s\n' %
+                        binary)
+                    g.write('\tapt install %s=%s\n' % (
+                            binary, build_version))
+            g.write('\n\n')
+        elif os.path.exists(os.path.join(run_dir, build_log_path)):
+            include_console_log(
+                g, os.path.join(run_dir, build_log_path),
+                FAIL_BUILD_LOG_TAIL)
+        else:
+            include_console_log(
+                g, os.path.join(run_dir, worker_log_path))
+        if os.path.exists(os.path.join(run_dir, build_log_path)):
+            g.write('`Full build log <%s>`_\n' %
+                    build_log_path)
+        elif os.path.exists(
+                os.path.join(run_dir, worker_log_path)):
+            g.write('`Full worker log <%s>`_\n' %
+                    worker_log_path)
+        g.write("\n")
+        g.write("*Last Updated: " + time.asctime() + "*\n")
+
+
 with open(os.path.join(dir, 'index.rst'), 'w') as indexf:
     indexf.write("""\
 Package Index
@@ -77,9 +162,10 @@ Package Index
 
             f.write('Recent package builds\n')
             f.write('---------------------\n')
+            runs = list(state.iter_runs(name))
             for (run_id, (start_time, finish_time), command, description,
                     package_name, merge_proposal_url, build_version,
-                    build_distro, result_code) in state.iter_runs(name):
+                    build_distro, result_code) in runs_by_pkg.get(name, []):
                 if build_version is None:
                     continue
                 f.write('* %s (for %s)' % (build_version, build_distro))
@@ -93,7 +179,7 @@ Package Index
 
             for (run_id, (start_time, finish_time), command, description,
                     package_name, merge_proposal_url, build_version,
-                    build_distro, result_code) in state.iter_runs(name):
+                    build_distro, result_code) in runs_by_pkg.get(name, []):
                 kind = command.split(' ')[0]
                 f.write('* `%s: %s <%s/>`_' % (
                     finish_time.isoformat(timespec='minutes'), kind, run_id))
@@ -102,80 +188,6 @@ Package Index
                 if result_code:
                     f.write(' => %s' % result_code)
                 f.write('\n')
-
-                run_dir = os.path.join(pkg_dir, run_id)
-                if not os.path.isdir(run_dir):
-                    os.mkdir(run_dir)
-
-                with open(os.path.join(run_dir, 'index.rst'), 'w') as g:
-                    g.write('Run of %s for %s\n' % (kind, package_name))
-                    g.write('====' + len(run_id) * '=' + '\n')
-
-                    g.write('* Package: `%s <..>`_\n' % package_name)
-                    g.write('* Start time: %s\n' % start_time)
-                    g.write('* Finish time: %s\n' % finish_time)
-                    g.write('* Run time: %s\n' % (finish_time - start_time))
-                    g.write('* Description: %s\n' % description)
-                    if build_version:
-                        changes_name = changes_filename(
-                            package_name, build_version,
-                            get_build_architecture())
-                        g.write('* Changes filename: `%s '
-                                '<../../../%s/%s>`_\n'
-                                % (changes_name, build_distro, changes_name))
-                    g.write('\n')
-                    g.write('Try this locally::\n\n\t')
-                    # TODO(jelmer): Don't put lintian-fixer specific code here
-                    svp_args = command.split(' ')
-                    if svp_args[0] == 'lintian-brush':
-                        g.write('debian-svp lintian-brush %s %s' % (
-                            name, ' '.join(
-                                ['--fixers=%s' % f for f in svp_args[1:]])))
-                    elif svp_args[0] == 'new-upstream':
-
-                        g.write('debian-svp new-upstream %s' % name)
-                        if svp_args[1:]:
-                            g.write(' ' +  ' '.join(svp_args[1:]))
-                    else:
-                        raise AssertionError
-                    g.write('\n\n')
-                    build_log_path = 'build.log'
-                    worker_log_path = 'worker.log'
-                    if build_version:
-                        changes_name = changes_filename(
-                            package_name, build_version,
-                            get_build_architecture())
-                        changes_path = os.path.join(
-                            "../public_html", build_distro, changes_name)
-                        if not os.path.exists(changes_path):
-                            warning('Missing changes path %r', changes_path)
-                        else:
-                            g.write('Install this package (if you have the ')
-                            g.write('`apt repository <../../../>`_ enabled) '
-                                    'by running one of::\n\n')
-                            for binary in changes_get_binaries(changes_path):
-                                g.write(
-                                    '\tapt install -t upstream-releases %s\n' %
-                                    binary)
-                                g.write('\tapt install %s=%s\n' % (
-                                        binary, build_version))
-                        g.write('\n\n')
-                    elif os.path.exists(os.path.join(run_dir, build_log_path)):
-                        include_console_log(
-                            g, os.path.join(run_dir, build_log_path),
-                            FAIL_BUILD_LOG_TAIL)
-                    else:
-                        include_console_log(
-                            g, os.path.join(run_dir, worker_log_path))
-                    if os.path.exists(os.path.join(run_dir, build_log_path)):
-                        g.write('`Full build log <%s>`_\n' %
-                                build_log_path)
-                    elif os.path.exists(
-                            os.path.join(run_dir, worker_log_path)):
-                        g.write('`Full worker log <%s>`_\n' %
-                                worker_log_path)
-                    g.write("\n")
-                    g.write("*Last Updated: " + time.asctime() + "*\n")
 
     indexf.write("\n")
     indexf.write("*Last Updated: " + time.asctime() + "*\n")
