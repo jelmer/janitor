eacece36b4c75b77767d9cc486f82ac3b8dbc253
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-05-05 17:19:20 +0000

    Fix duration type.

--- state.sql
+++ state.sql
@@ -59,6 +59,6 @@ CREATE TABLE IF NOT EXISTS queue (
    foreign key (package) references package(name),
    -- Some subworker-specific indication of what we are expecting to do.
    context text,
-   estimated_duration integer,
+   estimated_duration interval,
    unique(package, command, mode)
 );
