6f99b18eca8787a5a22d7ebf2437556ecc8a9d02
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-08 23:57:57 +0000

    Add contact page.

--- site2/generate.py
+++ site2/generate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(
     loader=FileSystemLoader('templates'),
@@ -8,4 +10,10 @@ env = Environment(
 
 template = env.get_template('index.html')
 with open('html/index.html', 'w') as f:
+    f.write(template.render())
+
+os.makedirs('html/contact', exist_ok=True)
+
+template = env.get_template('contact.html')
+with open('html/contact/index.html', 'w') as f:
     f.write(template.render())
