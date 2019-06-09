29932e8f6943ccc54e4cf9a85a40844583ecd0c1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-09 01:44:53 +0000

    Add fresh-snapshots/fresh-releases.

---
+++ site2/report-apt-repo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import argparse
+from debian.changelog import Version
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from janitor import state, udd  # noqa: E402
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+
+parser = argparse.ArgumentParser(prog='report-apt-repo')
+parser.add_argument("suite")
+args = parser.parse_args()
+
+
+def get_unstable_versions(present):
+    unstable = {}
+    if present:
+        for package in udd.UDD.public_udd_mirror().get_source_packages(
+                packages=list(present), release='sid'):
+            unstable[package.name] = Version(package.version)
+    return unstable
+
+
+def gather_package_list():
+    present = {}
+    for source, version in state.iter_published_packages(args.suite):
+        present[source] = Version(version)
+
+    unstable = get_unstable_versions(present)
+
+    for source in sorted(present):
+        yield (
+            source,
+            present[source].upstream_version,
+            unstable[source].upstream_version
+            if source in unstable else '')
+
+
+template = env.get_template(args.suite + '.html')
+sys.stdout.write(template.render(packages=gather_package_list()))
