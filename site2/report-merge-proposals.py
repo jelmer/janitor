f91e2679dddb271e23fd00224d3c845ce290b20b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-09 01:00:40 +0000

    List merge proposals.

---
+++ site2/report-merge-proposals.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+parser = argparse.ArgumentParser('report-state')
+parser.add_argument('name', nargs='?', type=str, default=None)
+args = parser.parse_args()
+
+from janitor import state  # noqa: E402
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+
+proposals_by_status = {}
+for url, status, package in state.iter_all_proposals(branch_name=args.name):
+    proposals_by_status.setdefault(status, []).append(url)
+
+
+template = env.get_template('merge-proposals.html')
+sys.stdout.write(template.render(
+        open_proposals=proposals_by_status.get('open', []),
+        merged_proposals=proposals_by_status.get('merged', []),
+        closed_proposals=proposals_by_status.get('closed', [])))
