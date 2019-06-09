3230cd89fa100584707f2f6b5e67ddbda3553eab
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-09 00:14:36 +0000

    Add lintian-fixes index page.

--- site2/generate.py
+++ site2/generate.py
@@ -20,3 +20,10 @@ for dest, src in simple_render.items():
     os.makedirs(os.path.join('html', os.path.dirname(dest)), exist_ok=True)
     with open(os.path.join('html', dest), 'w') as f:
         f.write(template.render())
+
+
+template = env.get_template('lintian-fixes.html')
+os.makedirs(os.path.join('html/lintian-fixes'), exist_ok=True)
+with open(os.path.join('html', 'lintian-fixes', 'index.html'), 'w') as f:
+    import lintian_brush
+    f.write(template.render({'lintian_brush': lintian_brush}))
