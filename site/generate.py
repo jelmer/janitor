ef23077f0a3635b9ce8c34dfc831de3129bd0e19
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-10 23:13:00 +0000

    Fix formatting, log location.

--- site/generate.py
+++ site/generate.py
@@ -2,13 +2,14 @@
 
 import os
 
+import argparse
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-import argparse
 parser = argparse.ArgumentParser(prog='generate')
 parser.add_argument("directory")
 args = parser.parse_args()
@@ -24,13 +25,15 @@ simple_render = {
 
 for dest, src in simple_render.items():
     template = env.get_template(src)
-    os.makedirs(os.path.join(args.directory, os.path.dirname(dest)), exist_ok=True)
+    os.makedirs(
+        os.path.join(args.directory, os.path.dirname(dest)), exist_ok=True)
     with open(os.path.join(args.directory, dest), 'w') as f:
         f.write(template.render())
 
 
 template = env.get_template('lintian-fixes.html')
-os.makedirs(os.path.join(args.directory, 'lintian-fixes'), exist_ok=True)
-with open(os.path.join(args.directory, 'lintian-fixes', 'index.html'), 'w') as f:
+lintian_fixes_dir = os.path.join(args.directory, 'lintian-fixes')
+os.makedirs(lintian_fixes_dir, exist_ok=True)
+with open(os.path.join(lintian_fixes_dir, 'index.html'), 'w') as f:
     import lintian_brush
     f.write(template.render({'lintian_brush': lintian_brush}))
--- site/report-merge-proposals.py
+++ site/report-merge-proposals.py
@@ -4,6 +4,8 @@ import argparse
 import sys
 import os
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 sys.path.insert(0, os.path.dirname(__file__))
 
 parser = argparse.ArgumentParser('report-state')
@@ -12,7 +14,6 @@ args = parser.parse_args()
 
 from janitor import state  # noqa: E402
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(
     loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html', 'xml'])
