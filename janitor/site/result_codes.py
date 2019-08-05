3267e1848dfef564b45e9548044157316cbc16d6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-05 20:06:37 +0000

    Drop obsolete code.

--- janitor/site/result_codes.py
+++ janitor/site/result_codes.py
@@ -7,15 +7,6 @@ from janitor import state
 from janitor.site import env
 
 
-async def get_results_by_code(code):
-    by_code = {}
-    for (source, command, result_code, log_id,
-         description, start_time, duration) in await state.iter_last_runs():
-        by_code.setdefault(result_code, []).append(
-            (source, command, log_id, description))
-    return by_code.get(code, [])
-
-
 async def generate_result_code_page(code, entries):
     template = env.get_template('result-code.html')
     return await template.render_async(code=code, entries=entries)
@@ -36,19 +27,3 @@ async def generate_result_code_index(by_code):
 async def write_result_code_index(path, by_code):
     with open(os.path.join(args.path, 'index.html'), 'w') as f:
         f.write(await generate_result_code_index(by_code))
-
-
-if __name__ == '__main__':
-    import argparse
-    import asyncio
-    parser = argparse.ArgumentParser(prog='report-result-codes')
-    parser.add_argument(
-        'path', type=str, default='result-codes', help='Output path')
-    args = parser.parse_args()
-
-    loop = asyncio.get_event_loop()
-    by_code = loop.run_until_complete(get_results_by_code())
-    jobs = [write_result_code_index(args.path, by_code)]
-    for code, items in by_code.items():
-        jobs.append(write_result_code_page(args.path, code, items))
-    loop.run_until_complete(asyncio.gather(*jobs))
