14d27c7a606bffa1501f6bd9ceeda9edb12654e6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-14 21:46:35 +0000

    Fix error message.

--- janitor/fix_build.py
+++ janitor/fix_build.py
@@ -278,7 +278,7 @@ def fix_missing_file(tree, error, committer=None):
 def fix_missing_perl_module(tree, error, committer=None):
     path = os.path.join('/usr/share/perl5', error.filename)
     package = get_package_for_paths([path], regex=False)
-    warning('no perl package found for %s (%r)', error.module, error.path)
+    warning('no perl package found for %s (%r)', error.module, path)
     if package is None:
         return False
     return add_build_dependency(tree, package, committer=committer)
