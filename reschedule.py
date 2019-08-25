7482fe3425ac245b3982efb27360362fbd392183
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-25 20:03:05 +0000

    Set estimated duration.

--- reschedule.py
+++ reschedule.py
@@ -28,7 +28,8 @@ async def main(result_code):
         print('Rescheduling %s, %s' % (package, suite))
         await state.add_to_queue(
             packages[package].branch_url,
-            package, command.split(' '), suite)
+            package, command.split(' '), suite,
+            estimated_duration=duration)
 
 
 asyncio.run(main(args.result_code))
