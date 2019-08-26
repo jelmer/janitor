e53e68d9aa8ebea2d590245e149c6470f4068ac8
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-26 23:01:28 +0000

    Fix reschedule.

--- reschedule.py
+++ reschedule.py
@@ -2,10 +2,12 @@
 
 import argparse
 import asyncio
+import re
 from janitor import state
 
 parser = argparse.ArgumentParser('reschedule')
 parser.add_argument('result_code', type=str)
+parser.add_argument('description_re', type=str, nargs='?')
 args = parser.parse_args()
 
 
@@ -17,10 +19,13 @@ async def main(result_code):
         packages[package.name] = package
 
     async for (package, suite, command, id, description, start_time,
-            duration, branch_url) in results:
+            duration, branch_url) in state.iter_last_runs(result_code):
         if package not in packages:
             continue
         if packages[package].branch_url is None:
+            continue
+        if (args.description_re and
+                not re.match(args.description_re, description, re.S)):
             continue
         print('Rescheduling %s, %s' % (package, suite))
         await state.add_to_queue(
