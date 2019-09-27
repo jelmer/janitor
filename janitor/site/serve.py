802acb526d9264428cf6470edb2a29b89ab2e066
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-09-27 22:38:59 +0000

    Add failed fixers page.

--- janitor/site/serve.py
+++ janitor/site/serve.py
@@ -212,6 +212,13 @@ if __name__ == '__main__':
             content_type='text/html', text=text,
             headers={'Cache-Control': 'max-age=600'})
 
+    async def handle_failed_lintian_fixes(request):
+        from .lintian_fixes import generate_failing_fixers_list
+        text = await generate_failing_fixers_list()
+        return web.Response(
+            content_type='text/html', text=text,
+            headers={'Cache-Control': 'max-age=600'})
+
     async def handle_run(request):
         from .pkg import generate_run_file
         from .. import state
@@ -408,6 +415,7 @@ if __name__ == '__main__':
     app.router.add_get('/cupboard/pkg/', handle_pkg_list)
     app.router.add_get('/cupboard/pkg/{pkg}/', handle_pkg)
     app.router.add_get('/cupboard/pkg/{pkg}/{run_id}/', handle_run)
+    app.router.add_get('/cupboard/lintian-fixes/failed-fixers', handle_failed_lintian_fixes)
     app.router.add_get(
         '/cupboard/pkg/{pkg}/{run_id}/{log:.*\\.log(\\.[0-9]+)?}', handle_log)
     app.router.add_get(
