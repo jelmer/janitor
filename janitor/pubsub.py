a8e46b3ec11a30838a84f178b5ae2b49105457eb
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-23 02:01:41 +0000

    Website fixes.

--- janitor/pubsub.py
+++ janitor/pubsub.py
@@ -15,7 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from asyncio import web
+import aiohttp
+from aiohttp import web
 import asyncio
 import json
 
--- janitor/site/publish.py
+++ janitor/site/publish.py
@@ -9,9 +9,3 @@ async def write_history(conn, limit=None):
     return await template.render_async(
         count=limit,
         history=state.iter_publish_history(conn, limit=limit))
-
-
-async def write_publish(package, branch_name, main_branch_revision, revision,
-                        mode, merge_proposal_url, result_code, description):
-    # For now..
-    return result_code
