66a0ef840e9b8df473a30977c013811d0ecd8b23
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-28 22:11:05 +0000

    Fix publish stuff.

--- janitor/policy.py
+++ janitor/policy.py
@@ -51,7 +51,7 @@ def apply_policy(config, suite, package_name, maintainer, uploaders):
                 not any([matches(m, package_name, maintainer, uploaders)
                          for m in policy.match])):
             continue
-        if field is not None and getattr(policy, field) is not None:
+        if field is not None and policy.HasField(field):
             mode = getattr(policy, field)
         if policy.changelog is not None:
             update_changelog = policy.changelog
