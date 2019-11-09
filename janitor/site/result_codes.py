78385287238b7867068cd838b5d5922157c932be
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-11-09 14:29:45 +0000

    Allow filtering by suite.

--- janitor/site/result_codes.py
+++ janitor/site/result_codes.py
@@ -5,14 +5,14 @@ import operator
 from janitor.site import env
 
 
-async def generate_result_code_page(code, entries):
+async def generate_result_code_page(code, entries, suite):
     template = env.get_template('result-code.html')
-    return await template.render_async(code=code, runs=entries)
+    return await template.render_async(code=code, runs=entries, suite=suite)
 
 
-async def generate_result_code_index(by_code, never_processed):
+async def generate_result_code_index(by_code, never_processed, suite):
     template = env.get_template('result-code-index.html')
 
     data = sorted(by_code, key=operator.itemgetter(1), reverse=True)
     data.append(('never-processed', never_processed))
-    return await template.render_async(result_codes=data)
+    return await template.render_async(result_codes=data, suite=suite)
