1e1841352b4f8d99d7e16c8dbeba4b2781b7d2e6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-05 20:31:57 +0000

    Avoid unnecessary queries to package table.

--- janitor/schedule.py
+++ janitor/schedule.py
@@ -305,11 +305,11 @@ async def do_schedule(conn, package, suite, offset=None,
     if offset is None:
         offset = DEFAULT_SCHEDULE_OFFSET
     (unused_publish_policy, update_changelog, compat_release) = (
-        await state.get_publish_policy(conn, package.name, suite))
+        await state.get_publish_policy(conn, package, suite))
     command = full_command(suite, update_changelog, compat_release)
-    estimated_duration = await estimate_duration(conn, package.name, suite)
+    estimated_duration = await estimate_duration(conn, package, suite)
     await state.add_to_queue(
-        conn, package.name, command, suite, offset,
+        conn, package, command, suite, offset,
         estimated_duration=estimated_duration, refresh=refresh,
         requestor=requestor)
     return offset, estimated_duration
