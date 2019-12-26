4c10b080ff65aa316541c8ace96850698e387c2e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-26 14:03:28 +0000

    Rely on tracking branches.

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -3,7 +3,7 @@ DEPS="lintian-brush silver-platter breezy"
 for NAME in $DEPS
 do
     pushd $NAME
-    git pull origin master
+    git pull
     popd
 done
 git commit -m "Update dependencies." $DEPS
