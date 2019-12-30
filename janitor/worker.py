d11e66a06e5e6256c106f151cd3cf28d8e1eb58e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-30 13:15:35 +0000

    Don't check for nothing-new-to-do when doing unchanged builds.

--- janitor/worker.py
+++ janitor/worker.py
@@ -676,8 +676,8 @@ def process_package(vcs_url, env, command, output_directory,
             if not ws.changes_since_main():
                 raise WorkerFailure('nothing-to-do', 'Nothing to do.')
 
-        if ws.resume_branch and not ws.changes_since_resume():
-            raise WorkerFailure('nothing-to-do', 'Nothing new to do.')
+            if ws.resume_branch and not ws.changes_since_resume():
+                raise WorkerFailure('nothing-to-do', 'Nothing new to do.')
 
         try:
             run_post_check(ws.local_tree, post_check_command, ws.orig_revid)
