8f88a4987257f5733ac45b87b1bf1197dd40c483
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-01-08 03:19:09 +0000

    Fix merged_at handling.

--- janitor/publish.py
+++ janitor/publish.py
@@ -760,17 +760,18 @@ async def check_existing(conn, rate_limiter, vcs_manager, topic_merge_proposal,
             except (NotImplementedError, AttributeError):
                 merged_by = None
             try:
-                merged_at = mp.get_merged_at()
+                merged_at = mp.get_merged_at().replace(tzinfo=None)
             except (NotImplementedError, AttributeError):
                 merged_at = None
         else:
             merged_by = None
             merged_at = None
         await state.set_proposal_info(
-            conn, mp.url, status, revision, package_name, merged_by, merged_at)
+            conn, mp.url, status, revision, package_name, merged_by,
+            merged_at)
         topic_merge_proposal.publish(
            {'url': mp.url, 'status': status, 'package': package_name,
-            'merged_by': merged_by, 'merged_at': merged_at})
+            'merged_by': merged_by, 'merged_at': str(merged_at)})
 
     for hoster, mp, status in iter_all_mps():
         status_count[status] += 1
