e3d1de7712e9134231ffad6a897e8fe1b761a0c7
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-01-12 21:53:01 +0000

    Add multiarch site.

--- janitor/site/multiarch_hints.py
+++ janitor/site/multiarch_hints.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 
+from .common import generate_pkg_context
 from janitor.site import (
     env,
     )
 
 
 SUITE = 'multiarch-fixes'
+
+
+async def generate_pkg_file(db, policy, client, archiver_url, publisher_url,
+                            package, run_id=None):
+    kwargs = await generate_pkg_context(
+        db, SUITE, policy, client, archiver_url, publisher_url, package,
+        run_id=run_id)
+    template = env.get_template('multiarch-fixes-package.html')
+    return await template.render_async(**kwargs)
 
 
 async def render_start():
