b951f1683ec5edd69ec80ed38e2738f88f7b3778
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-03-29 01:39:33 +0000

    Add stats.

---
+++ janitor/site/stats.py
@@ -0,0 +1,13 @@
+
+from . import env
+from .. import state
+
+async def write_stats(conn):
+    template = env.get_template('stats.html')
+
+    by_hoster = {}
+    for hoster, status, count in await state.get_hoster_merge_proposal_stats(conn):
+        by_hoster.setdefault(hoster, {})[status] = count
+
+    return await template.render_async(
+        by_hoster=by_hoster)
