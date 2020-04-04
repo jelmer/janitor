2b4c1afbf2326ead3ab8b1b1b8f51c378823a68f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-04-04 18:02:47 +0000

    Add some graphs.

--- janitor/site/result_codes.py
+++ janitor/site/result_codes.py
@@ -5,14 +5,18 @@ import operator
 from janitor.site import env
 
 
-async def generate_result_code_page(code, entries, suite):
+async def generate_result_code_page(code, entries, suite, all_suites):
     template = env.get_template('result-code.html')
-    return await template.render_async(code=code, runs=entries, suite=suite)
+    return await template.render_async(
+        code=code, runs=entries, suite=suite, all_suites=all_suites)
 
 
-async def generate_result_code_index(by_code, never_processed, suite):
+async def generate_result_code_index(by_code, never_processed, suite, all_suites):
     template = env.get_template('result-code-index.html')
 
-    data = sorted(by_code, key=operator.itemgetter(1), reverse=True)
+    data = [[code, count]
+            for (code, count) in
+            sorted(by_code, key=operator.itemgetter(1), reverse=True)]
     data.append(('never-processed', never_processed))
-    return await template.render_async(result_codes=data, suite=suite)
+    return await template.render_async(
+        result_codes=data, suite=suite, all_suites=all_suites)
