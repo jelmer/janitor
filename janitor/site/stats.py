f4f5f75cea395f64c8b653342e9cab6dc7211f10
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-04-04 18:02:47 +0000

    Add some graphs.

--- janitor/site/stats.py
+++ janitor/site/stats.py
@@ -6,10 +6,17 @@ from .. import state
 async def write_stats(conn):
     template = env.get_template('stats.html')
 
+    by_status = {}
     by_hoster = {}
     for hoster, status, count in await state.get_hoster_merge_proposal_stats(
             conn):
         by_hoster.setdefault(hoster, {})[status] = count
+        by_status.setdefault(status, {})[hoster] = count
+
+    by_status_chart = [
+        {'name': status, 'data': data}
+        for (status, data) in by_status.items()]
 
     return await template.render_async(
-        by_hoster=by_hoster)
+        by_hoster=by_hoster,
+        by_status_chart=by_status_chart)
