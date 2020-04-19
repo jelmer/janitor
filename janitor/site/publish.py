be78c4c5394ef9e17de606a3724d4b6fa50be5e7
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-04-19 15:20:50 +0000

    Include timestamp in publish history.

--- janitor/site/publish.py
+++ janitor/site/publish.py
@@ -4,8 +4,26 @@ from janitor import state
 from janitor.site import env
 
 
+async def iter_publish_history(conn, limit=None):
+    query = """
+SELECT
+    publish.timestamp, publish.package, publish.branch_name,
+    publish.main_branch_revision, publish.revision, publish.mode,
+    publish.merge_proposal_url, publish.result_code, publish.description,
+    package.vcs_browse
+FROM
+    publish
+JOIN package ON publish.package = package.name
+ORDER BY timestamp DESC
+"""
+    if limit:
+        query += " LIMIT %d" % limit
+    for row in await conn.fetch(query):
+        yield row
+
+
 async def write_history(conn, limit=None):
     template = env.get_template('publish-history.html')
     return await template.render_async(
         count=limit,
-        history=state.iter_publish_history(conn, limit=limit))
+        history=iter_publish_history(conn, limit=limit))
