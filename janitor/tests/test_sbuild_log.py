d93c00895f01b02f40c110ac6393583c3c70d36e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-04-21 02:54:59 +0000

    Fix parsing of autopkgtest section.

--- janitor/tests/test_sbuild_log.py
+++ janitor/tests/test_sbuild_log.py
@@ -892,21 +892,23 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
 
     def test_no_match(self):
         self.assertEqual(
-            (None, None, None, None),
+            (1, 'blalblala\n', None, None),
             find_autopkgtest_failure_description(['blalblala\n']))
 
     def test_unknown_error(self):
         self.assertEqual(
-            (1, 'python-bcolz', None, 'Test python-bcolz failed: some error'),
+            (2, 'python-bcolz', None, 'Test python-bcolz failed: some error'),
             find_autopkgtest_failure_description(
-                ['python-bcolz         FAIL some error\n']))
+                ['autopkgtest [07:58:03]: @@@@@@@@@@@@@@@@@@@@ summary\n',
+                 'python-bcolz         FAIL some error\n']))
 
     def test_timed_out(self):
         error = AutopkgtestTimedOut()
         self.assertEqual(
-            (1, 'unit-tests', error, 'timed out'),
+            (2, 'unit-tests', error, 'timed out'),
             find_autopkgtest_failure_description(
-                ['unit-tests           FAIL timed out']))
+                ['autopkgtest [07:58:03]: @@@@@@@@@@@@@@@@@@@@ summary\n',
+                 'unit-tests           FAIL timed out']))
 
     def test_deps(self):
         error = AutopkgtestDepsUnsatisfiable(
@@ -930,13 +932,14 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
               'bcolz_1.2.1+ds2-4~jan+lint1.dsc')])
 
         self.assertEqual(
-            (1, 'python-bcolz', error,
+            (2, 'python-bcolz', error,
              'Test python-bcolz failed: Test dependencies are unsatisfiable. '
              'A common reason is that your testbed is out of date '
              'with respect to the archive, and you need to use a '
              'current testbed or run apt-get update or use -U.'),
             find_autopkgtest_failure_description(
-                ['python-bcolz         FAIL badpkg\n',
+                ['autopkgtest [07:58:03]: @@@@@@@@@@@@@@@@@@@@ summary\n',
+                 'python-bcolz         FAIL badpkg\n',
                  'blame: arg:/home/janitor/tmp/tmppvupofwl/build-area/'
                  'bcolz-doc_1.2.1+ds2-4~jan+lint1_all.deb deb:bcolz-doc '
                  'arg:/home/janitor/tmp/tmppvupofwl/build-area/python-'
@@ -961,12 +964,13 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
              (None, '/home/janitor/tmp/tmpgbn5jhou/'
               'build-area/cmake-extras_1.3+17.04.20170310-6~jan.dsc')])
         self.assertEqual(
-            (1, 'intltool', error,
+            (2, 'intltool', error,
              'Test intltool failed: Test dependencies are unsatisfiable. '
              'A common reason is that your testbed is out of date with '
              'respect to the archive, and you need to use a current testbed '
              'or run apt-get update or use -U.'),
             find_autopkgtest_failure_description([
+                'autopkgtest [07:58:03]: @@@@@@@@@@@@@@@@@@@@ summary\n',
                 'intltool             FAIL badpkg',
                 'blame: arg:/home/janitor/tmp/tmpgbn5jhou/build-area/cmake'
                 '-extras_1.3+17.04.20170310-6~jan+unchanged1_all.deb '
@@ -980,7 +984,7 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
     def test_stderr(self):
         error = AutopkgtestStderrFailure('some output')
         self.assertEqual(
-            (1, 'intltool', error,
+            (6, 'intltool', error,
              'Test intltool failed due to unauthorized stderr output: '
              'some output'),
             find_autopkgtest_failure_description([
@@ -993,10 +997,9 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
                 'intltool            FAIL stderr: some output',
                 ]))
         self.assertEqual(
-            (1, 'intltool', MissingCommand('ss'),
+            (2, 'intltool', MissingCommand('ss'),
              '/tmp/bla: 12: ss: not found'),
             find_autopkgtest_failure_description([
-                'intltool            FAIL stderr: /tmp/bla: 12: ss: not found',
                 'autopkgtest [20:49:00]: test intltool:'
                 '  - - - - - - - - - - stderr - - - - - - - - - -',
                 '/tmp/bla: 12: ss: not found',
@@ -1005,10 +1008,11 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
                 'intltool            FAIL stderr: /tmp/bla: 12: ss: not found',
                 ]))
         self.assertEqual(
-            (1, 'command10', MissingCommand('uptime'),
+            (2, 'command10', MissingCommand('uptime'),
              'Can\'t exec "uptime": No such file or directory at '
              '/usr/lib/nagios/plugins/check_uptime line 529.'),
             find_autopkgtest_failure_description([
+                'autopkgtest [07:58:03]: @@@@@@@@@@@@@@@@@@@@ summary\n',
                 'command10            FAIL stderr: Can\'t exec "uptime": '
                 'No such file or directory at '
                 '/usr/lib/nagios/plugins/check_uptime line 529.']))
@@ -1030,7 +1034,7 @@ class FindAutopkgtestFailureDescriptionTests(unittest.TestCase):
         error = AutopkgtestTestbedFailure(
             'testbed auxverb failed with exit code 255')
         self.assertEqual(
-            (5, 'phpunit', error, None),
+            (4, 'phpunit', error, None),
             find_autopkgtest_failure_description("""\
 Removing autopkgtest-satdep (0) ...
 autopkgtest [06:59:00]: test phpunit: [-----------------------
@@ -1045,7 +1049,9 @@ Exiting with 16
 
     def test_test_command_failure(self):
         self.assertEqual(
-            (None, None, None, None),
+            (7, 'command2',
+             MissingFile('/usr/share/php/Pimple/autoload.php'),
+             'Cannot open file "/usr/share/php/Pimple/autoload.php".'),
             find_autopkgtest_failure_description("""\
 Removing autopkgtest-satdep (0) ...
 autopkgtest [01:30:11]: test command2: phpunit --bootstrap /usr/autoload.php
@@ -1062,4 +1068,74 @@ autopkgtest [01:30:12]: @@@@@@@@@@@@@@@@@@@@ summary
 command1             PASS
 command2             FAIL non-zero exit status 1
 Exiting with 4
-"""))
+""".splitlines(True)))
+
+    def test_dpkg_failure(self):
+        self.assertEqual(
+            (8, 'runtestsuite', None,
+'"dpkg --unpack /tmp/autopkgtest.hdIETy/4-autopkgtest-satdep.deb" ' \
+'failed with stderr "W: /var/lib/schroot/session/unstable-amd64-' \
+'sbuild-7fb1b836-14f9-4709-8584-cbbae284db97: '\
+'Failed to stat file: No such file or directory'),
+            find_autopkgtest_failure_description("""\
+autopkgtest [19:19:19]: test require: [-----------------------
+autopkgtest [19:19:20]: test require: -----------------------]
+autopkgtest [19:19:20]: test require:  \
+- - - - - - - - - - results - - - - - - - - - -
+require              PASS
+autopkgtest [19:19:20]: test runtestsuite: preparing testbed
+Get:1 file:/tmp/autopkgtest.hdIETy/binaries  InRelease
+Ign:1 file:/tmp/autopkgtest.hdIETy/binaries  InRelease
+autopkgtest [19:19:23]: ERROR: "dpkg --unpack \
+/tmp/autopkgtest.hdIETy/4-autopkgtest-satdep.deb" failed with \
+stderr "W: /var/lib/schroot/session/unstable-amd64-sbuild-\
+7fb1b836-14f9-4709-8584-cbbae284db97: Failed to stat file: \
+No such file or directory
+""".splitlines(True)))
+
+    def test_last_stderr_line(self):
+        self.assertEqual(
+            (11, 'unmunge', None,
+             'Test unmunge failed: non-zero exit status 2'),
+            find_autopkgtest_failure_description("""\
+autopkgtest [17:38:49]: test unmunge: [-----------------------
+munge: Error: Failed to access "/run/munge/munge.socket.2": \
+No such file or directory
+unmunge: Error: No credential specified
+autopkgtest [17:38:50]: test unmunge: -----------------------]
+autopkgtest [17:38:50]: test unmunge: \
+ - - - - - - - - - - results - - - - - - - - - -
+unmunge              FAIL non-zero exit status 2
+autopkgtest [17:38:50]: test unmunge: \
+ - - - - - - - - - - stderr - - - - - - - - - -
+munge: Error: Failed to access "/run/munge/munge.socket.2": \
+No such file or directory
+unmunge: Error: No credential specified
+autopkgtest [17:38:50]: @@@@@@@@@@@@@@@@@@@@ summary
+unmunge              FAIL non-zero exit status 2
+Exiting with 4
+""".splitlines(True)))
+
+    def test_python_error_in_output(self):
+        self.assertEqual(
+            (7, 'unit-tests-3', None,
+             'builtins.OverflowError: mktime argument out of range'),
+            find_autopkgtest_failure_description("""\
+autopkgtest [14:55:35]: test unit-tests-3: [-----------------------
+  File "twisted/test/test_log.py", line 511, in test_getTimezoneOffsetWithout
+    self._getTimezoneOffsetTest("Africa/Johannesburg", -7200, -7200)
+  File "twisted/test/test_log.py", line 460, in _getTimezoneOffsetTest
+    daylight = time.mktime(localDaylightTuple)
+builtins.OverflowError: mktime argument out of range
+-------------------------------------------------------------------------------
+Ran 12377 tests in 143.490s
+
+143.4904797077179 12377 12377 1 0 2352
+autopkgtest [14:58:01]: test unit-tests-3: -----------------------]
+autopkgtest [14:58:01]: test unit-tests-3: \
+ - - - - - - - - - - results - - - - - - - - - -
+unit-tests-3         FAIL non-zero exit status 1
+autopkgtest [14:58:01]: @@@@@@@@@@@@@@@@@@@@ summary
+unit-tests-3         FAIL non-zero exit status 1
+Exiting with 4
+""".splitlines(True)))
