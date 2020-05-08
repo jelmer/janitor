867b20f297d29f4122ef1335ca767b0e61836d56
Jelmer Vernooĳ <jelmer@debian.org>
2020-05-08 22:07:47 +0000

    Identify CVS protocol URLs.

--- janitor/worker.py
+++ janitor/worker.py
@@ -462,6 +462,8 @@ class NewUpstreamWorker(SubWorker):
                 if 'Unsupported protocol for url' in str(e.error):
                     if 'svn://' in str(e.error):
                         error_code = 'upstream-unsupported-vcs-svn'
+                    elif 'cvs+server://' in str(e.error):
+                        error_code = 'upstream-unsupported-vcs-cvs'
                     else:
                         error_code = 'upstream-unsupported-vcs'
                 raise WorkerFailure(error_code, error_description)
