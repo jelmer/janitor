03a9d5eef5f0bd0ea33f8b99966378be00a4f107
Jelmer Vernooĳ <jelmer@debian.org>
2020-05-25 14:13:28 +0000

    Use offset 1 when rescheduling transient error runs.

--- janitor/publish.py
+++ janitor/publish.py
@@ -929,7 +929,7 @@ async def check_existing_mp(
                  'Rescheduling.', mp.url, last_run.result_code)
             await state.add_to_queue(
                 conn, last_run.package, shlex.split(last_run.command),
-                last_run.suite, offset=0, refresh=False,
+                last_run.suite, offset=1, refresh=False,
                 requestor='publisher (transient error)')
         else:
             note('%s: Last run failed (%s). Not touching merge proposal.',
