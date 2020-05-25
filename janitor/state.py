0637898a91b9e72a6c7720831dd504e3279d7b6e
Jelmer Vernooĳ <jelmer@debian.org>
2020-05-25 14:26:41 +0000

    Order publish ready by value.

--- janitor/state.py
+++ janitor/state.py
@@ -937,6 +937,7 @@ LEFT JOIN package ON package.name = run.package
 LEFT JOIN publish_policy ON
     publish_policy.package = run.package AND publish_policy.suite = run.suite
 WHERE result_code = 'success' AND result IS NOT NULL
+ORDER BY value DESC
 """
     if suites is not None:
         query += " AND run.suite = ANY($1::text[]) "
