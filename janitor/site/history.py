d8c1f8b0608c593cbe6982d992641d47c2aa39cf
Jelmer Vernooĳ <jelmer@debian.org>
2020-06-02 01:00:13 +0000

    Allow viewing history by worker.

--- janitor/site/history.py
+++ janitor/site/history.py
@@ -4,8 +4,8 @@ from janitor import state
 from janitor.site import env
 
 
-async def write_history(conn, limit=None):
+async def write_history(conn, worker=None, limit=None):
     template = env.get_template('history.html')
     return await template.render_async(
         count=limit,
-        history=state.iter_runs(conn, limit=limit))
+        history=state.iter_runs(conn, worker=worker, limit=limit))
