d5042108f423aac5eaed03292eedd6ff8990e5b3
Jelmer Vernooĳ <jelmer@debian.org>
2020-06-02 01:11:02 +0000

    Import hoster plugins rather than propose plugin.

--- cleanup.py
+++ cleanup.py
@@ -10,7 +10,9 @@ import breezy.git  # noqa: F401
 from breezy.propose import (
     hosters,
     )
-import breezy.plugins.propose  # noqa: F401
+import breezy.plugins.gitlab  # noqa: F401
+import breezy.plugins.github  # noqa: F401
+import breezy.plugins.launchpad  # noqa: F401
 
 
 def projects_to_remove(instance):
