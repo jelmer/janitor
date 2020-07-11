1317d5dca3856481616f31cf9fb25afaf4cd15fa
Jelmer Vernooĳ <jelmer@debian.org>
2020-07-11 18:14:37 +0000

    allow passing in metadata from custom scripts.

--- schedule.sh
+++ schedule.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter:$(pwd)/breezy" python3 -m janitor.package_metadata "$@"
-PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter:$(pwd)/breezy" python3 -m janitor.candidates "$@"
-PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter:$(pwd)/breezy" python3 -m janitor.schedule --policy=policy.conf "$@"
+export PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter:$(pwd)/breezy"
+./udd-package-metadata.py | python3 -m janitor.package_metadata "$@"
+python3 -m janitor.candidates "$@"
+python3 -m janitor.schedule --policy=policy.conf "$@"
