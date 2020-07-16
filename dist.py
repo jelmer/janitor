57364b0dac07ea28ba6fd458a5b85f3ccf89b723
Jelmer Vernooĳ <jelmer@debian.org>
2020-07-16 21:19:59 +0000

    Fix syntax.

--- dist.py
+++ dist.py
@@ -37,7 +37,7 @@ if os.path.exists('setup.py'):
     if os.stat('setup.py').st_mode & stat.S_IEXEC:
         sys.exit(subprocess.call(['./setup.py', 'sdist']))
     else:
-        sys.exit(subprocess.call(['python3', './setup.py', 'sdist'])
+        sys.exit(subprocess.call(['python3', './setup.py', 'sdist']))
 if os.path.exists('dist.ini') and not os.path.exists('Makefile.PL'):
     with open('dist.ini', 'rb') as f:
         for line in f:
