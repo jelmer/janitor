843a206677a1e4d5c8b4b3763a7796561baf9fd2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-08-04 21:26:53 +0000

    Drop some obsolete files.

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt install brz lintian-brush brz-debian protobuf-compiler python3-github python3-gitlab python3-launchpadlib
-gpg --recv-keys 6F915003D1998D6A
-gpg --export 6F915003D1998D6A > ~/debian-janitor/janitor.gpg
-
--- schroot/setup-agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-INCLUDE="cython3 devscripts python3-aiohttp python3-all-dev python3-certifi python3-configobj python3-debian python3-dulwich python3-distro-info python3-fastimport python3-iniparse python3-launchpadlib python3-levenshtein python3-paramiko python3-patiencediff python3-pkginfo python3-pyinotify python3-ruamel.yaml python3-setuptools python3-six python3-subunit python3-testtools python3-urllib3 sbuild gnome-pkg-tools postgresql-server-dev-all lintian dos2unix gpg libdebhelper-perl git"
-
-if [ ! -d /srv/chroot/jenkins ]; then
-sudo chroot /srv/chroot/jenkins apt install $INCLUDE
