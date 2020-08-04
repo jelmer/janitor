b535b4db0932405c2651438ac4dcb459257d3a54
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-08-04 21:26:53 +0000

    Drop some obsolete files.

--- schroot/create-agent-schroot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo sbuild-createchroot --chroot-prefix jenkins --include=eatmydata,ccache,gnupg $SUITE /srv/chroot/jenkins http://deb.debian.org/debian --extra-repository="deb http://deb.debian.org/debian $SUITE-backports main"
-
-sudo chroot /srv/chroot/jenkins ./setup-agent.sh
--- sign-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -f Release.gpg.tmp InRelease.tmp
-echo "$PASSPHRASE" | gpg --no-tty --batch --detach-sign -o Release.gpg.tmp "$1"
-mv Release.gpg.tmp Release.gpg
-echo "$PASSPHRASE" | gpg --no-tty --batch --clearsign -o InRelease.tmp "$1"
-mv InRelease.tmp InRelease
-
