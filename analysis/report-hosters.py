87396b36b4c8eae406630bf39a790e0bb93f4aac
Jelmer Vernooĳ <jelmer@debian.org>
2020-09-20 18:28:31 +0000

    Include vcs as well.

--- analysis/report-hosters.py
+++ analysis/report-hosters.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 import asyncio
-
 import operator
+from typing import Dict
+from urllib.parse import urlparse
 
 import matplotlib.pyplot as plt
-
-from urllib.parse import urlparse
 
 from silver_platter.debian import vcs_field_to_bzr_url_converters
 
@@ -32,21 +31,29 @@ renames = {
 loop = asyncio.get_event_loop()
 udd = loop.run_until_complete(UDD.public_udd_mirror())
 all = loop.run_until_complete(udd._conn.fetch(
-    "SELECT source, vcs, url FROM vcswatch GROUP by 1, 2, 3"))
-hosters = {}
+    "SELECT sources.source, vcs, url FROM sources "
+    "LEFT JOIN vcswatch ON vcswatch.source = sources.source "
+    "WHERE release = 'sid' GROUP by 1, 2, 3"))
+hosters: Dict[str, int] = {}
 url_converters = dict(vcs_field_to_bzr_url_converters)
-for row in all:
-    try:
-        converter = url_converters[row[1]]
-    except KeyError:
-        continue
-    host = urlparse(row[2])[1]
-    host = host.split(':')[0]
-    if '@' in host:
-        host = host.split('@')[1]
-    host = renames.get(host, host)
-    hosters.setdefault(host, 0)
-    hosters[host] += 1
+for source, vcs, url in all:
+    if vcs is None:
+        name = 'no vcs'
+    else:
+        try:
+            converter = url_converters[vcs]
+        except KeyError:
+            name = 'unknown vcs %s' % vcs
+        else:
+            url = converter(url)
+            host = urlparse(url)[1]
+            host = host.split(':')[0]
+            if '@' in host:
+                host = host.split('@')[1]
+            host = renames.get(host, host)
+            name = '%s (%s)' % (host, vcs)
+    hosters.setdefault(name, 0)
+    hosters[name] += 1
 
 ordered_hosters = list(sorted(
     hosters.items(), key=operator.itemgetter(1),
@@ -58,7 +65,7 @@ with open('hosters.csv', 'w') as f:
         f.write('%s,%d\n' % (host, count))
 
 ordered_hosters = (
-    ordered_hosters[:5] + [
+    ordered_hosters[:10] + [
         ('other', sum(map(operator.itemgetter(1), ordered_hosters[5:])))])
 
 labels = list(map(operator.itemgetter(0), ordered_hosters))
