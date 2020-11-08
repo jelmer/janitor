f83c645f3eeee7aedcbee19fe3b747a1eb2db23c
Jelmer Vernooĳ <jelmer@debian.org>
2020-11-08 15:00:25 +0000

    Stop uploading from the runner.

--- janitor/state.py
+++ janitor/state.py
@@ -728,10 +728,11 @@ WHERE
 
 async def iter_published_packages(conn: asyncpg.Connection, suite):
     return await conn.fetch("""
-select distinct on (package.name) package.name, build_version, archive_version
-from run left join package on package.name = run.package
-where run.build_distribution = $1 and not package.removed
-order by package.name, build_version desc
+select distinct on (package.name) package.name, debian_build.version,
+archive_version from debian_build
+left join package on package.name = debian_build.source
+where debian_build.distribution = $1 and not package.removed
+order by package.name, debian_build.version desc
 """, suite)
 
 
