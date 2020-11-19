b4a79b15c0228b19726f49c10367b210486c507d
Jelmer Vernooĳ <jelmer@debian.org>
2020-11-19 22:23:47 +0000

    Fix SQL query.

--- janitor/state.py
+++ janitor/state.py
@@ -1674,7 +1674,7 @@ async def get_site_session(conn: asyncpg.Connection, session_id: str) -> Any:
 async def has_cotenants(
         conn: asyncpg.Connection, package: str, url: str) -> Optional[bool]:
     rows = await conn.fetch(
-        'SELECT package FROM package where branch_url = $1', url)
+        'SELECT name FROM package where branch_url = $1', url)
     if len(rows) > 1:
         return True
     elif len(rows) == 1 and rows[0][0] == package:
