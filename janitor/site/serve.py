51bb5b047f355d82942abe2c617c5c9351e8def0
Jelmer Vernooĳ <jelmer@debian.org>
2021-02-02 03:00:20 +0000

    More support for scrub-obsolete.

--- janitor/site/serve.py
+++ janitor/site/serve.py
@@ -770,6 +770,26 @@ order by url, last_run.finish_time desc
             raise web.HTTPNotFound()
 
     @html_template(
+        'scrub-obsolete-package.html',
+        headers={'Cache-Control': 'max-age=600'})
+    async def handle_scrub_obsolete_pkg(request):
+        from .common import generate_pkg_context
+        # TODO(jelmer): Handle Accept: text/diff
+        pkg = request.match_info['pkg']
+        run_id = request.match_info.get('run_id')
+        try:
+            return await generate_pkg_context(
+                request.app.database,
+                request.app.config,
+                'scrub-obsolete',
+                request.app.policy,
+                request.app.http_client_session,
+                request.app.differ_url,
+                request.app.vcs_store_url, pkg, run_id)
+        except KeyError:
+            raise web.HTTPNotFound()
+
+    @html_template(
         'multiarch-fixes-package.html',
         headers={'Cache-Control': 'max-age=600'})
     async def handle_multiarch_fixes_pkg(request):
@@ -1141,6 +1161,12 @@ order by url, last_run.finish_time desc
     app.router.add_get(
         '/lintian-fixes/stats', handle_lintian_fixes_stats,
         name='lintian-fixes-stats')
+    app.router.add_get(
+        '/scrub-obsolete/pkg/{pkg}/', handle_scrub_obsolete_pkg,
+        name='scrub-obsolete-package')
+    app.router.add_get(
+        '/scrub-obsolete/pkg/{pkg}/{run_id}', handle_scrub_obsolete_pkg,
+        name='scrub-obsolete-package-run')
     NEW_UPSTREAM_REGEX = 'fresh-(releases|snapshots)'
     app.router.add_get(
         '/{suite:%s}/' % NEW_UPSTREAM_REGEX, handle_apt_repo,
