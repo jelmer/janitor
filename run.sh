40d44d17e64dbd79483786aa3154ed3ee0d6b9a4
Jelmer Vernooĳ <jelmer@debian.org>
2021-02-05 01:00:56 +0000

    Add upstream-ontologist to path.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/silver-platter:$(pwd)/breezy:$(pwd)/python-debian/lib:$(pwd)/ognibuild" python3 -m janitor.runner "$@"
+PYTHONPATH="$PYTHONPATH:$(pwd)/lintian-brush:$(pwd)/upstream-ontologist:$(pwd)/silver-platter:$(pwd)/breezy:$(pwd)/python-debian/lib:$(pwd)/ognibuild" python3 -m janitor.runner "$@"
