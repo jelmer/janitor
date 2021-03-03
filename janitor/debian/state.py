ff9831960f5dc516b9e6b62879edad18243da5b3
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-03 15:31:53 +0000

    Move to debian/.

--- janitor/debian/state.py
+++ janitor/debian/state.py
@@ -439,3 +439,16 @@ where rb.revision = $1 and run.package is not null
     if len(rows) == 1:
         return rows[0][0], rows[0][1]
     return None, None
+
+
+async def iter_published_packages(conn: asyncpg.Connection, suite):
+    return await conn.fetch(
+        """
+select distinct on (package.name) package.name, debian_build.version,
+archive_version from debian_build
+left join package on package.name = debian_build.source
+where debian_build.distribution = $1 and not package.removed
+order by package.name, debian_build.version desc
+""",
+        suite,
+    )
--- janitor/site/apt_repo.py
+++ janitor/site/apt_repo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-from janitor import state
+from janitor.debian import state
 
 
 async def gather_package_list(conn, suite):
-    present = await state.iter_published_packages(conn, suite)
+    present = await debian_state.iter_published_packages(conn, suite)
 
     for source, build_version, archive_version in sorted(present):
         yield (source, build_version, archive_version)
