1ffe318c0b0f1777362733b0b1345ce19b9ca155
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-05 17:42:21 +0000

    Look at JANITOR_CREDENTIALS environment variable.

--- pull_worker.sh
+++ pull_worker.sh
@@ -6,5 +6,13 @@ export BRZ_PLUGINS_AT=debian@$WD/breezy-debian
 export SBUILD_CONFIG=${SBUILD_CONFIG:-$WD/sbuildrc}
 export AUTOPKGTEST=$WD/autopkgtest-wrapper
 
+
+
 export PYTHONPATH=$WD/ognibuild:$WD:$WD/breezy:$WD/silver-platter:$WD/lintian-brush:$WD/dulwich:$WD/debmutate:$WD/python-debian/lib:$WD/upstream-ontologist:$WD/buildlog-consultant
-python3 -m janitor.pull_worker "$@"
+
+if [ -n "${JANITOR_CREDENTIALS}" ];
+then
+   python3 -m janitor.pull_worker --credentials="${JANITOR_CREDENTIALS}" "$@"
+else
+   python3 -m janitor.pull_worker "$@"
+fi
