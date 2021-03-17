656e89901bacade5ddac0ea7f1d193d412b7ca8d
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-17 17:19:48 +0000

    Fix dependencies script.

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -3,7 +3,11 @@ DEPS="lintian-brush silver-platter breezy dulwich breezy-debian python-debian de
 for NAME in $DEPS
 do
     pushd $NAME
-    git pull
+    if [ "$NAME" = "ognibuild" ]; then
+        git pull origin main
+    else
+       git pull origin master
+    fi
     popd
 done
 git commit -m "Update dependencies." $DEPS
