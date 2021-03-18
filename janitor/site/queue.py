489bc03b405e3f373fc3f4ad399cc3328fcd86b8
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-18 18:08:06 +0000

    Fix changer subcommand.

--- janitor/site/queue.py
+++ janitor/site/queue.py
@@ -21,7 +21,7 @@ import shlex
 from typing import AsyncIterator, Tuple, Optional, Dict, Any, Iterator
 
 from janitor import state
-from janitor.worker import changer_subcommand
+from janitor.worker import debian_changer_subcommand, generic_changer_subcommand
 
 
 def lintian_tag_link(tag: str) -> str:
@@ -118,7 +118,10 @@ async def get_queue(
                     map(lintian_tag_link, row["context"].split(" "))
                 )
         else:
-            cs = changer_subcommand(command[0])
+            try:
+                cs = debian_changer_subcommand(command[0])
+            except KeyError:
+                cs = generic_changer_subcommand(command[0])
             description = cs.describe_command(command)
         if only_command is not None:
             description = expecting or ""
