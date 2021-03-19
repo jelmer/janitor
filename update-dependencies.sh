c36baf64347e30ed4b61ecc428f7e82831c26716
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-19 16:22:27 +0000

    Update buildlog-consultant.

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEPS="lintian-brush silver-platter breezy dulwich breezy-debian python-debian debmutate ognibuild upstream-ontologist"
+DEPS="lintian-brush silver-platter breezy dulwich breezy-debian python-debian debmutate ognibuild upstream-ontologist buildlog-consultant"
 for NAME in $DEPS
 do
     pushd $NAME
