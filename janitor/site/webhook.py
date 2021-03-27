777b891a9184a031c3a03f5d82605bf0ca84dd2f
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-27 17:01:40 +0000

    Don't handle ping requests.

--- janitor/site/webhook.py
+++ janitor/site/webhook.py
@@ -149,11 +149,11 @@ async def process_webhook(request, db):
             # TODO(jelmer: If nothing found, then maybe fall back to
             # urlutils.basename(body['project']['path_with_namespace'])?
         elif "X-GitHub-Event" in request.headers:
-            if request.headers["X-GitHub-Event"] not in ("ping", "push"):
+            if request.headers["X-GitHub-Event"] not in ("push", ):
                 return web.json_response({}, status=200)
             urls = get_branch_urls_from_github_webhook(body)
         elif "X-Launchpad-Event-Type" in request.headers:
-            if request.headers["X-Launchpad-Event-Type"] not in ("ping", "bzr:push:0.1", "git:push:0.1"):
+            if request.headers["X-Launchpad-Event-Type"] not in ("bzr:push:0.1", "git:push:0.1"):
                 return web.json_response({}, status=200)
             if request.headers["X-Launchpad-Event-Type"] == 'bzr:push:0.1':
                 urls = get_bzr_branch_urls_from_launchpad_webhook(body)
