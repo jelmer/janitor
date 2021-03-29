ce81b523a8910e5dfa21607c8521cdc9ffc46666
Jelmer Vernooĳ <jelmer@debian.org>
2021-03-29 21:59:29 +0100

    Fix run access.

--- janitor/differ.py
+++ janitor/differ.py
@@ -199,7 +199,7 @@ async def get_run_pair(db, old_id, new_id):
             text="missing artifacts", headers={"unavailable_run_id": old_id}
         )
 
-    if new_run is None or new_run.result_code != 'success':
+    if new_run is None or new_run['result_code'] != 'success':
         raise web.HTTPNotFound(
             text="missing artifacts", headers={"unavailable_run_id": new_id}
         )
