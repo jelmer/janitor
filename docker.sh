76ee98d7e6bc841770e0b338bf7b042778ae6699
Jelmer Vernooĳ <jelmer@debian.org>
2021-05-07 21:49:13 +0100

    Stop forwarding.

--- docker.sh
+++ docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for NAME in base site runner publish archive differ worker vcs_store postgres irc_notify mastodon_notify xmpp_notify
+if [ ! -z "$@" ]; then
+    TODO="$@"
+else
+    TODO="base site runner publish archive differ worker vcs_store postgres irc_notify mastodon_notify xmpp_notify"
+fi
+for NAME in $TODO
 do
    docker build -t eu.gcr.io/debian-janitor/$NAME -f Dockerfile_$NAME .
    docker push eu.gcr.io/debian-janitor/$NAME
