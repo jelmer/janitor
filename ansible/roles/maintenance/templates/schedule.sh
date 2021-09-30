1e893eeedffb7b9c8020869f2ad2789933c7d8e7
Jelmer Vernooĳ <jelmer@debian.org>
2021-09-30 01:22:32 +0100

    Support multiple commands.

--- ansible/roles/maintenance/templates/schedule.sh
+++ ansible/roles/maintenance/templates/schedule.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 {% for distro in janitor_distributions %}
-{{ distro.package_metadata_command }} | python3 -m janitor.package_metadata \
+( {{ distro.package_metadata_command }} ) | python3 -m janitor.package_metadata \
 	--config={{ janitor_conf_path}} \
 	--distribution={{ distro.name }} \
 	--package-overrides={{ janitor_code_path }}/package_overrides.conf \
