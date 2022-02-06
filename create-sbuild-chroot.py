fb6c7d45331a358d88cef1f8f0a38dca2fac846b
Jelmer Vernooĳ <jelmer@debian.org>
2022-02-06 22:28:57 +0000

    Allow building multiple chroots.

--- create-sbuild-chroot.py
+++ create-sbuild-chroot.py
@@ -36,59 +36,64 @@ parser.add_argument(
     default=[])
 parser.add_argument('--base-directory', type=str, help='Base directory for chroots')
 parser.add_argument('--user', type=str, help='User to create home directory for')
-parser.add_argument('--make-sbuild-tarball', type=str, help='Create sbuild tarball')
+parser.add_argument('--make-sbuild-tarball', action='store_true', help='Create sbuild tarball')
 parser.add_argument(
     "--config", type=str, default="janitor.conf", help="Path to configuration."
 )
-parser.add_argument("distribution", type=str)
+parser.add_argument("distribution", type=str, nargs='*')
 args = parser.parse_args()
 
 with open(args.config, "r") as f:
     config = read_config(f)
 
-try:
-    distro_config = get_distribution(config, args.distribution)
-except KeyError:
-    parser.error('no such distribution: %s' % args.distribution)
+for distribution in (args.distribution or [d.name for d in config.distribution]):
+    try:
+        distro_config = get_distribution(config, args.distribution)
+    except KeyError:
+        parser.error('no such distribution: %s' % args.distribution)
 
-sbuild_arch = get_sbuild_architecture()
-if not args.base_directory:
-    parser.print_usage()
-    parser.exit()
-sbuild_path = os.path.join(args.base_directory, distro_config.chroot)
+    sbuild_arch = get_sbuild_architecture()
+    if not args.base_directory:
+        parser.print_usage()
+        parser.exit()
+    sbuild_path = os.path.join(args.base_directory, distro_config.chroot)
 
-if args.remove_old:
-    for entry in os.scandir('/etc/schroot/chroot.d'):
-        cp = RawConfigParser()
-        cp.read([entry.path])
-        if distro_config.chroot in cp.sections():
-            old_sbuild_path = cp.get(
-                '%s-%s-sbuild' % (distro_config.name, sbuild_arch),
-                'directory')
-            if old_sbuild_path != sbuild_path:
-                raise AssertionError('sbuild path has changed: %s != %s' % (
-                    old_sbuild_path, sbuild_path))
-            if os.path.isdir(old_sbuild_path):
-                shutil.rmtree(old_sbuild_path)
-            os.unlink(entry.path)
+    if args.remove_old:
+        for entry in os.scandir('/etc/schroot/chroot.d'):
+            cp = RawConfigParser()
+            cp.read([entry.path])
+            if distro_config.chroot in cp.sections():
+                old_sbuild_path = cp.get(
+                    '%s-%s-sbuild' % (distro_config.name, sbuild_arch),
+                    'directory')
+                if old_sbuild_path != sbuild_path:
+                    raise AssertionError('sbuild path has changed: %s != %s' % (
+                        old_sbuild_path, sbuild_path))
+                if os.path.isdir(old_sbuild_path):
+                    shutil.rmtree(old_sbuild_path)
+                os.unlink(entry.path)
 
-suites = []
-for suite in config.suite:
-    if not suite.HasField('debian_build'):
-        continue
-    if suite.debian_build.base_distribution != distro_config.name:
-        continue
-    suites.append(suite.debian_build.build_distribution)
-for campaign in config.campaign:
-    if not campaign.HasField('debian_build'):
-        continue
-    if campaign.debian_build.base_distribution != distro_config.name:
-        continue
-    suites.append(campaign.debian_build.build_distribution)
-create_chroot(distro_config, sbuild_path, suites, sbuild_arch, args.include, make_sbuild_tarball=args.make_sbuild_tarball)
+    suites = []
+    for suite in config.suite:
+        if not suite.HasField('debian_build'):
+            continue
+        if suite.debian_build.base_distribution != distro_config.name:
+            continue
+        suites.append(suite.debian_build.build_distribution)
+    for campaign in config.campaign:
+        if not campaign.HasField('debian_build'):
+            continue
+        if campaign.debian_build.base_distribution != distro_config.name:
+            continue
+        suites.append(campaign.debian_build.build_distribution)
+    if args.make_sbuild_tarball:
+        make_sbuild_tarball = os.path.join(args.base_directory, distro_config.chroot + '.tar.gz')
+    else:
+        make_sbuild_tarball = None
+    create_chroot(distro_config, sbuild_path, suites, sbuild_arch, args.include, make_sbuild_tarball=None)
 
-if args.user:
-    subprocess.check_call(
-        ['schroot', '-c', '%s-%s-sbuild' % (distro_config.name, sbuild_arch),
-         '--directory', '/', '--', 'install', '-d', '--owner=%s' % args.user,
-         pwd.getpwnam(args.user).pw_dir])
+    if args.user:
+        subprocess.check_call(
+            ['schroot', '-c', '%s-%s-sbuild' % (distro_config.name, sbuild_arch),
+             '--directory', '/', '--', 'install', '-d', '--owner=%s' % args.user,
+             pwd.getpwnam(args.user).pw_dir])
