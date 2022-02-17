e277c225a79c7c21d5359d66a95823ef3613f473
Jelmer Vernooĳ <jelmer@debian.org>
2022-02-17 23:39:53 +0000

    Support missing package overrides.

--- ansible/roles/maintenance/templates/schedule.sh
+++ ansible/roles/maintenance/templates/schedule.sh
@@ -3,7 +3,7 @@
 ( {{ distro.package_metadata_command }} ) | python3 -m janitor.package_metadata \
 	--config={{ janitor_conf_path}} \
 	--distribution={{ distro.name }} \
-	--package-overrides={% if janitor_package_overrides %}{{ janitor_package_overrides }}{% else %}/dev/null{% endif %}\
+	--package-overrides={% if janitor_package_overrides %}{{ janitor_home }}/package_overrides.conf{% else %}/dev/null{% endif %}\
 	"$@"
 {% endfor %}
 ( {{ janitor_candidates_command }} ) | python3 -m janitor.candidates --config={{ janitor_conf_path}} "$@"
