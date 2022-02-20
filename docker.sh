a57050be4411178946a14d9b73317f4a392ddb20
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-02-20 18:00:18 +0000

    Publish to github registry.

--- docker.sh
+++ docker.sh
@@ -3,6 +3,7 @@ TODO="$@"
 TODO=${TODO:-base site runner publish archive differ worker vcs_store postgres irc_notify mastodon_notify xmpp_notify}
 for NAME in $TODO
 do
-   docker build -t eu.gcr.io/debian-janitor/$NAME -f Dockerfile_$NAME .
+   docker build -t eu.gcr.io/debian-janitor/$NAME -t ghcr.io/jelmer/janitor/$NAME -f Dockerfile_$NAME .
    docker push eu.gcr.io/debian-janitor/$NAME
+   docker push ghcr.io/jelmer/janitor/$NAME
 done
