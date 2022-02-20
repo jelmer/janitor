355ed5af6020b15f8beb181b8086e0c2bd20a569
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-02-20 16:28:46 +0000

    Update copyright year.

--- janitor/worker.py
+++ janitor/worker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (C) 2018 Jelmer Vernooij <jelmer@jelmer.uk>
+# Copyright (C) 2018-2022 Jelmer Vernooij <jelmer@jelmer.uk>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
