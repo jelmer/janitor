10659537ab2331ce76ad3b40366f6d65d865517a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-02-27 13:01:37 +0000

    FIx some more bugs in the website, allow inspecting individual publish/merge-proposal events.

--- janitor/site/merge_proposals.py
+++ janitor/site/merge_proposals.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+from typing import Optional
+
 import asyncpg
 
 
 async def get_proposals_with_run(
-    conn: asyncpg.Connection, suite: str
+    conn: asyncpg.Connection, suite: Optional[str]
 ):
     query = """
 SELECT
@@ -17,10 +19,17 @@ FROM
     merge_proposal
 LEFT JOIN new_result_branch ON new_result_branch.revision = merge_proposal.revision
 LEFT JOIN run ON run.id = new_result_branch.run_id
+"""
+    args = []
+    if suite:
+        args.append(suite)
+        query += """
 WHERE suite = $1
+"""
+    query += """
 ORDER BY merge_proposal.url, run.finish_time DESC
 """
-    return await conn.fetch(query, suite)
+    return await conn.fetch(query, *args)
 
 
 async def write_merge_proposals(db, suite):
@@ -37,4 +46,31 @@ async def write_merge_proposals(db, suite):
         "open_proposals": proposals_by_status.get("open", []),
         "merged_proposals": merged,
         "closed_proposals": proposals_by_status.get("closed", []),
+    }
+
+
+async def get_proposal_with_run(conn: asyncpg.Connection, url: str):
+    query = """
+SELECT
+    run.package AS package,
+    run.suite AS suite,
+    merge_proposal.url AS url,
+    merge_proposal.status AS status,
+    merge_proposal.merged_at AS merged_at,
+    merge_proposal.merged_by AS merged_by
+FROM
+    merge_proposal
+LEFT JOIN new_result_branch ON new_result_branch.revision = merge_proposal.revision
+LEFT JOIN run ON run.id = new_result_branch.run_id
+WHERE url = $1
+"""
+    return await conn.fetchrow(query, url)
+
+
+async def write_merge_proposal(db, url):
+    async with db.acquire() as conn:
+        proposal = await get_proposal_with_run(conn, url)
+
+    return {
+        "proposal": proposal,
     }
--- janitor/site/publish.py
+++ janitor/site/publish.py
@@ -4,7 +4,7 @@
 async def iter_publish_history(conn, limit=None):
     query = """
 SELECT
-    publish.timestamp, publish.package, publish.branch_name,
+    publish.id, publish.timestamp, publish.package, publish.branch_name,
     publish.mode, publish.merge_proposal_url, publish.result_code,
     publish.description, package.vcs_browse
 FROM
@@ -21,4 +21,30 @@ async def write_history(conn, limit=None):
     return {
         "count": limit,
         "history": await iter_publish_history(conn, limit=limit),
+    }
+
+
+async def get_publish(conn, id):
+    query = """
+SELECT
+    publish.id AS id,
+    publish.timestamp AS timestamp,
+    publish.package AS package,
+    publish.branch_name AS branch_name,
+    publish.mode AS mode,
+    publish.merge_proposal_url AS merge_proposal_url,
+    publish.result_code AS result_code,
+    publish.description AS description,
+    package.vcs_browse AS vcs_browse
+FROM
+    publish
+JOIN package ON publish.package = package.name
+WHERE id = $1
+"""
+    return await conn.fetchrow(query, id)
+
+
+async def write_publish(conn, id):
+    return {
+        "publish": await get_publish(conn, id),
     }
