b0af0704f0c55dcea2414cf47537e15385cf4322
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-03-02 01:52:52 +0000

    Fix site.

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@ TODO="$@"
 TODO=${TODO:-base site runner publish archive differ worker vcs_store irc_notify mastodon_notify xmpp_notify}
 for NAME in $TODO
 do
-   docker build -t eu.gcr.io/debian-janitor/$NAME -t ghcr.io/jelmer/janitor/$NAME -f Dockerfile_$NAME .
-   docker push eu.gcr.io/debian-janitor/$NAME
-   docker push ghcr.io/jelmer/janitor/$NAME
+   docker build -t eu.gcr.io/debian-janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:latest -f Dockerfile_$NAME .
+   docker push eu.gcr.io/debian-janitor/$NAME:latest
+   docker push ghcr.io/jelmer/janitor/$NAME:latest
 done
