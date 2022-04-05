cf17cbee27d64c597461b2064ee9cb844601c62e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-04-05 18:46:55 +0100

    Tag with git sha.

--- docker.sh
+++ docker.sh
@@ -3,6 +3,8 @@ TODO="$@"
 TODO=${TODO:-base site runner publish archive differ worker vcs_store irc_notify mastodon_notify xmpp_notify}
 for NAME in $TODO
 do
-   docker build -t ghcr.io/jelmer/janitor/$NAME:latest -f Dockerfile_$NAME .
+   SHA=$(git rev-parse HEAD)
+   docker build -t ghcr.io/jelmer/janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:$SHA -f Dockerfile_$NAME .
    docker push ghcr.io/jelmer/janitor/$NAME:latest
+   docker push ghcr.io/jelmer/janitor/$NAME:$SHA
 done
