a4a6146e587aa4ee64f46aefaab682a0c3f4c427
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-04-18 20:55:32 +0100

    add --tee argument to worker

--- run_worker.sh
+++ run_worker.sh
@@ -9,4 +9,4 @@ export AUTOPKGTEST=$WD/autopkgtest-wrapper
 export PATH=$WD/debmutate/scripts:$WD/breezy-debian/scripts:$PATH
 export PYTHONPATH=$WD/ognibuild:$WD:$WD/breezy:$WD/silver-platter:$WD/lintian-brush:$WD/dulwich:$WD/debmutate:$WD/python-debian/lib:$WD/upstream-ontologist:$WD/buildlog-consultant
 
-python3 -m janitor.worker "$@"
+python3 -m janitor.worker --tee "$@"
