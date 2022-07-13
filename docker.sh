ebc57f4ad1dcac17ea99c0c6f4929c9748fd98aa
Jelmer Vernooĳ <jelmer@debian.org>
2022-07-13 19:23:15 +0100

    add counter.

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ TODO=${TODO:-base site runner publish archive worker vcs_store_git vcs_store_bzr
 for NAME in $TODO
 do
    SHA=$(git rev-parse HEAD)
-   docker build -t ghcr.io/jelmer/janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:$SHA -f Dockerfile_$NAME .
+   docker build --no-cache -t ghcr.io/jelmer/janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:$SHA -f Dockerfile_$NAME .
    docker push ghcr.io/jelmer/janitor/$NAME:latest
    docker push ghcr.io/jelmer/janitor/$NAME:$SHA
 done
