5623b4ffcf2f4f4b677c36ddebefa7e4e88446f4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-08-30 02:16:18 +0100

    Switch to buildah.

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ TODO=${TODO:-base site runner publish archive worker git_store bzr_store irc_not
 for NAME in $TODO
 do
    SHA=$(git rev-parse HEAD)
-   docker build --no-cache -t ghcr.io/jelmer/janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:$SHA -f Dockerfile_$NAME .
-   docker push ghcr.io/jelmer/janitor/$NAME:latest
-   docker push ghcr.io/jelmer/janitor/$NAME:$SHA
+   buildah build -t ghcr.io/jelmer/janitor/$NAME:latest -t ghcr.io/jelmer/janitor/$NAME:$SHA -f Dockerfile_$NAME .
+   buildah push ghcr.io/jelmer/janitor/$NAME:latest
+   buildah push ghcr.io/jelmer/janitor/$NAME:$SHA
 done
