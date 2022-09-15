1e05009ccc3e240df3831cf045f0c891cb5534fe
Jelmer Vernooĳ <jelmer@debian.org>
2022-09-15 18:46:52 +0100

    Support setting command in candidates.

--- janitor/candidates.py
+++ janitor/candidates.py
@@ -36,6 +36,7 @@ def iter_candidates_from_script(stdin):
         yield (
             candidate.package,
             candidate.suite,
+            candidate.command,
             candidate.context,
             candidate.value,
             candidate.success_chance,
@@ -44,7 +45,7 @@ def iter_candidates_from_script(stdin):
 
 async def store_candidates(
         conn: asyncpg.Connection,
-        entries: List[Tuple[str, str, Optional[str], Optional[str], Optional[int],
+        entries: List[Tuple[str, str, Optional[str], Optional[str], Optional[str], Optional[int],
                             Optional[float]]]):
     """Store candidates.
 
@@ -55,11 +56,11 @@ async def store_candidates(
     """
     await conn.executemany(
         "INSERT INTO candidate "
-        "(package, suite, change_set, context, value, success_chance) "
-        "VALUES ($1, $2, $3, $4, $5, $6) "
+        "(package, suite, command, change_set, context, value, success_chance) "
+        "VALUES ($1, $2, $3, $4, $5, $6, $7) "
         "ON CONFLICT (package, suite, coalesce(change_set, ''::text)) "
         "DO UPDATE SET context = EXCLUDED.context, value = EXCLUDED.value, "
-        "success_chance = EXCLUDED.success_chance",
+        "success_chance = EXCLUDED.success_chance, command = EXCLUDED.command",
         entries,
     )
 
@@ -109,10 +110,11 @@ async def main():
 
         logging.info("Adding candidates.")
         proposed_candidates = [
-            (package, suite, None, context, value, success_chance)
+            (package, suite, command, None, context, value, success_chance)
             for (
                 package,
                 suite,
+                command,
                 context,
                 value,
                 success_chance,
