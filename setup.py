5b3ea54dcb0d83730dee362d6499db1b72a0703c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-10-15 11:17:27 +0100

    Build and install protobuf files.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 from setuptools import setup
+import setuptools.command.build
+setuptools.command.build.build.sub_commands.insert(
+    0, ('build_proto', lambda x: True))
 setup()
