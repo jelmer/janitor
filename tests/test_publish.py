2bacf4445abb1fdb635c33296ab82b8b47ff9231
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-10-26 09:33:39 +0100

    Add tests for publish

---
+++ tests/test_publish.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# Copyright (C) 2022 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+from janitor.publish import (
+    create_app,
+)
+
+
+async def create_client(aiohttp_client):
+    return await aiohttp_client(await create_app(
+        vcs_managers={}, db=None, redis=None,
+        config=None, differ_url="https://differ/"))
+
+
+async def test_health(aiohttp_client):
+    client = await create_client(aiohttp_client)
+
+    resp = await client.get("/health")
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "ok"
+
+
+async def test_ready(aiohttp_client):
+    client = await create_client(aiohttp_client)
+
+    resp = await client.get("/ready")
+    assert resp.status == 200
+    text = await resp.text()
+    assert text == "ok"
