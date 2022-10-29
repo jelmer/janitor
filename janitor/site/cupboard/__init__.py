46a5d766d24d5cdb5b3b5e6b11a04bca073be027
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-10-29 14:46:26 +0100

    More done page fixes.

--- janitor/site/cupboard/__init__.py
+++ janitor/site/cupboard/__init__.py
@@ -493,11 +493,13 @@ async def handle_ready_proposals(request):
 
 @html_template(env, "cupboard/done-list.html", headers={"Vary": "Cookie"})
 async def handle_done_proposals(request):
-    try:
-        since = datetime.fromisoformat(request.query["since"])
-    except ValueError as e:
-        raise web.HTTPBadRequest(text="invalid since") from e
-    except KeyError:
+    since_str = request.query.get("since")
+    if since_str:
+        try:
+            since = datetime.fromisoformat(since_str)
+        except ValueError as e:
+            raise web.HTTPBadRequest(text="invalid since") from e
+    else:
         since = None
 
     return await generate_done_list(request.app.database, since)
