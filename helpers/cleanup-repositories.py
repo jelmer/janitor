4f3a827829ba0a767c7f905e0354509ecef8312a
Jelmer Vernooij <jelmer@jelmer.uk>
2022-12-21 14:09:30 +0000

    Handle UnsupportedForge.

--- helpers/cleanup-repositories.py
+++ helpers/cleanup-repositories.py
@@ -6,6 +6,7 @@ This is necessary in particular because some hosting sites
 that a single user can own (in the case of GitLab, 1000).
 """
 
+import logging
 import sys
 
 import breezy
@@ -15,6 +16,7 @@ import breezy.git  # noqa: F401
 
 from breezy.forge import (
     iter_forge_instances,
+    UnsupportedForge,
 )
 import breezy.plugins.gitlab  # noqa: F401
 import breezy.plugins.github  # noqa: F401
@@ -39,11 +41,17 @@ def main(argv=None):
     parser.add_argument("--dry-run", action="store_true", help="Dry run.")
     args = parser.parse_args()
 
+    logging.basicConfig(format='%(message)s')
+
     for instance in iter_forge_instances():
-        for project in projects_to_remove(instance):
-            print("Deleting %s from %r" % (project, instance))
-            if not args.dry_run:
-                instance.delete_project(project)
+        try:
+            for project in projects_to_remove(instance):
+                logging.info("Deleting %s from %r" % (project, instance))
+                if not args.dry_run:
+                    instance.delete_project(project)
+        except UnsupportedForge as e:
+            logging.warning(
+                'Ignoring unsupported instance %s: %s', instance, e)
 
 
 if __name__ == "__main__":
