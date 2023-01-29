60767d002b6d1db05d26653e4bb74bf2eee26518
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-01-29 15:16:06 +0000

    Add tests for Queue.add / Queue.next_item

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -4,3 +4,12 @@ from janitor.queue import Queue
 async def test_get_buckets(con):
     queue = Queue(con)
     assert await queue.get_buckets() == []
+
+
+async def test_add(con):
+    queue = Queue(con)
+    await con.execute("INSERT INTO codebase (name) VALUES ('foo')")
+    assert await queue.add(codebase="foo", campaign="bar", command="true") == (1, 'default')
+    queue_item, vcs_info = await queue.next_item()
+    assert queue_item.codebase == 'foo'
+    assert queue_item.campaign == 'bar'
