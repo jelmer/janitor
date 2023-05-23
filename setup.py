173277f6315abe61481eecdcb1ecbcab73a37d24
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-05-23 16:17:51 +0000

    Move some mail filter code to rust

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from setuptools import setup
 from setuptools_protobuf import Protobuf
+from setuptools_rust import RustExtension, Binding
 
-setup(protobufs=[
-    Protobuf('janitor/config.proto', mypy=True),
-])
+setup(
+    protobufs=[Protobuf('janitor/config.proto', mypy=True)],
+    rust_extensions=[RustExtension(
+        "janitor._mail_filter", "crates/mail-filter-py/Cargo.toml",
+        binding=Binding.PyO3)],
+)
