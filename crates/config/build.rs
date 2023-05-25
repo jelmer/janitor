9f06bd084b3655a477c114c3a4c6291d0cc57a79
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-05-25 14:50:43 +0100

    Add config crate

---
+++ crates/config/build.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+
+fn main() {
+    let top_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap() + "/../..")
+        .canonicalize()
+        .unwrap();
+
+    protobuf_codegen::Codegen::new()
+        .cargo_out_dir("generated")
+        .inputs([top_dir.join("janitor/config.proto")])
+        .include(top_dir)
+        .run_from_script();
+}
