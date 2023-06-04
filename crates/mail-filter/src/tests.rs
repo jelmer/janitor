6f8e15235abf91ec53db780db09ce9887f2a3007
Jelmer Vernooij <jelmer@jelmer.uk>
2023-06-04 16:27:58 +0100

    Drop interface file

---
+++ crates/mail-filter/src/tests.rs
@@ -0,0 +1,16 @@
+use super::parse_email;
+use std::io::Read;
+
+#[test]
+fn test_parse_github_merged_email() {
+    let email = include_bytes!("../tests/data/github-merged-email.txt");
+
+    assert_eq!(Some("https://github.com/UbuntuBudgie/budgie-desktop/pull/78"), parse_email(std::io::Cursor::new(email)).as_deref());
+}
+
+#[test]
+fn test_parse_gitlab_merged_email() {
+    let email = include_bytes!("../tests/data/gitlab-merged-email.txt");
+
+    assert_eq!(Some("https://salsa.debian.org/debian/pkg-lojban-common/-/merge_requests/2"), parse_email(std::io::Cursor::new(email)).as_deref());
+}
