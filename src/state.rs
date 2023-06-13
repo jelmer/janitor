a80f0a5e62af1d327c5b22615aecae26eaa7e108
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-06-14 00:13:41 +0100

    Handle database settings through environment

--- src/state.rs
+++ src/state.rs
@@ -1,11 +1,15 @@
 use crate::config::Config;
-use sqlx::postgres::{PgPoolOptions, Postgres};
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions, Postgres};
 use sqlx::Pool;
 
 pub async fn create_pool(config: &Config) -> Result<Pool<Postgres>, sqlx::Error> {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(config.database_location.as_ref().unwrap())
-        .await?;
+    let pool_options = PgPoolOptions::new().max_connections(5);
+    let pool = if let Some(ref database_url) = config.database_location {
+        pool_options.connect(database_url).await?
+    } else {
+        let options = PgConnectOptions::new();
+        pool_options.connect_with(options).await?
+    };
+
     Ok(pool)
 }
