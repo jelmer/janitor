b10474fd87b3b537832e65a7eb5a31052dc54cbe
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-07-07 13:56:54 +0100

    Add init_logging function

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 pub mod config;
 pub mod state;
+
+pub fn init_logging(gcp_logging: bool, debug_mode: bool) {
+    if gcp_logging {
+        stackdriver_logger::init_with_cargo!("../Cargo.toml");
+    } else if debug_mode {
+        env_logger::init();
+    } else {
+        env_logger::builder()
+            .filter(None, log::LevelFilter::Info)
+            .init();
+    }
+}
