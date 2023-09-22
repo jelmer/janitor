8a16f59d811ce6fd889cb0b5b8c6173ae8451ec0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-09-22 17:10:00 +0100

    configure setuptools-protobuf from pyproject

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 from setuptools import setup
-from setuptools_protobuf import Protobuf
 from setuptools_rust import Binding, RustBin, RustExtension
 
 setup(
-    protobufs=[Protobuf('janitor/config.proto', mypy=True)],
     rust_extensions=[
         RustBin(
             "janitor-mail-filter", "crates/mail-filter/Cargo.toml",
