b4dd0cc95dfae9e109bb1ca910e9ef0d4c6c9865
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-09-22 17:22:05 +0100

    Migrate protobuf and rust config to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
 from setuptools import setup
-from setuptools_rust import Binding, RustBin, RustExtension
 
-setup(
-    rust_extensions=[
-        RustBin(
-            "janitor-mail-filter", "crates/mail-filter/Cargo.toml",
-            features=["cmdline"]),
-        RustBin(
-            "janitor-worker", "crates/worker/Cargo.toml",
-            features=["cli"]),
-        RustExtension(
-            "janitor._worker", "crates/worker-py/Cargo.toml",
-            binding=Binding.PyO3)]
-)
+setup()
