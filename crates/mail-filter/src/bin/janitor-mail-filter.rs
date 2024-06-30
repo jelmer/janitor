2a015154b541ea1ec0656687dfd994a3b2110a1a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-06-30 01:05:00 +0100

    Bump breezyshim

--- crates/mail-filter/src/bin/janitor-mail-filter.rs
+++ crates/mail-filter/src/bin/janitor-mail-filter.rs
@@ -1,56 +1,60 @@
-
 use std::fs::File;
 
-use std::process;
 use log::debug;
+use std::process;
 
-use clap::{Arg, App};
-use log::{error,info};
-use reqwest::blocking::{Client};
+use clap::Parser;
+use log::{error, info};
+use reqwest::blocking::Client;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(
+        short,
+        long,
+        default_value = "https://janitor.debian.net/api/refresh-proposal-status"
+    )]
+    refresh_url: String,
+    #[clap(short, long, default_value = "/dev/stdin")]
+    input: String,
+}
 
 fn refresh_merge_proposal(api_url: &str, merge_proposal_url: &str) -> Result<(), String> {
     let client = Client::new();
-    let res = client.post(api_url)
+    let res = client
+        .post(api_url)
         .json(&serde_json::json!({"url": merge_proposal_url}))
-        .send().map_err(|e| e.to_string())?;
+        .send()
+        .map_err(|e| e.to_string())?;
 
     match res.status().as_u16() {
         200 | 202 => Ok(()),
-        status => Err(format!("error {} triggering refresh for {}", status, api_url)),
+        status => Err(format!(
+            "error {} triggering refresh for {}",
+            status, api_url
+        )),
     }
 }
 
 fn main() {
-    let matches = App::new("Email Parser")
-        .arg(Arg::with_name("refresh-url")
-            .help("URL to submit requests to.")
-            .default_value("https://janitor.debian.net/api/refresh-proposal-status")
-            .takes_value(true))
-        .arg(Arg::with_name("input")
-            .help("Path to read mail from.")
-            .default_value("/dev/stdin")
-            .takes_value(true))
-        .get_matches();
+    let args = Args::parse();
 
-    let refresh_url = matches.value_of("refresh-url").unwrap();
-    let input = matches.value_of("input").unwrap();
-
-    let f = File::open(input).unwrap();
+    let f = File::open(args.input).unwrap();
 
     match janitor_mail_filter::parse_email(f) {
         Some(merge_proposal_url) => {
             info!("Found merge proposal URL: {}", merge_proposal_url);
-            match refresh_merge_proposal(refresh_url, &merge_proposal_url) {
+            match refresh_merge_proposal(&args.refresh_url, &merge_proposal_url) {
                 Ok(()) => process::exit(0),
                 Err(e) => {
                     error!("Error: {}", e);
                     process::exit(1);
-                },
+                }
             }
-        },
+        }
         None => {
             debug!("No merge proposal URL found.");
             process::exit(0);
-        },
+        }
     }
 }
