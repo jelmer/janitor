329b5d38a83e49091842d74a799b95c3c8e6cf8b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-12 12:20:57 +0100

    Add get_branch_vcs_type

--- src/vcs.rs
+++ src/vcs.rs
@@ -1,3 +1,8 @@
+use breezyshim::branch::Branch;
+use breezyshim::error::Error as BrzError;
+use pyo3::exceptions::PyAttributeError;
+use pyo3::prelude::*;
+
 pub fn is_authenticated_url(url: &url::Url) -> bool {
     ["git+ssh", "bzr+ssh"].contains(&url.scheme())
 }
@@ -16,4 +21,20 @@ mod is_authenticated_url_tests {
             &url::Url::parse("http://example.com").unwrap()
         ));
     }
+}
+
+pub fn get_branch_vcs_type(branch: &dyn Branch) -> Result<String, BrzError> {
+    let repository = branch.repository();
+    Python::with_gil(|py| {
+        let object = repository.to_object(py);
+        match object.getattr(py, "vcs") {
+            Ok(vcs) => vcs
+                .getattr(py, "abbreviation")
+                .unwrap()
+                .extract::<String>(py),
+            Err(e) if e.is_instance_of::<PyAttributeError>(py) => Ok("bzr".to_string()),
+            Err(e) => Err(e),
+        }
+    })
+    .map_err(|e| BrzError::from(e))
 }
