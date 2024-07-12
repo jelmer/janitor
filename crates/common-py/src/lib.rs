b2a7baf4a99e148eceeae139bcd8f35b1701c3a0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-12 22:12:04 +0100

    vcs: Move more code to rust

--- crates/common-py/src/lib.rs
+++ crates/common-py/src/lib.rs
@@ -1,7 +1,25 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn is_authenticated_url(url: &str) -> PyResult<bool> {
+    Ok(janitor::vcs::is_authenticated_url(
+        &url::Url::parse(url)
+            .map_err(|e| PyValueError::new_err((format!("Invalid URL: {}", e),)))?,
+    ))
+}
+
+#[pyfunction]
+fn is_alioth_url(url: &str) -> PyResult<bool> {
+    Ok(janitor::vcs::is_alioth_url(&url::Url::parse(url).map_err(
+        |e| PyValueError::new_err((format!("Invalid URL: {}", e),)),
+    )?))
+}
+
 #[pymodule]
-pub fn _common(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+pub fn _common(m: &Bound<PyModule>) -> PyResult<()> {
     pyo3_log::init();
+    m.add_function(wrap_pyfunction!(is_authenticated_url, m)?)?;
+    m.add_function(wrap_pyfunction!(is_alioth_url, m)?)?;
     Ok(())
 }
