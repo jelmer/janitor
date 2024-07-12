a72c6f15c5af72386b7754635dbd915a0aee48e2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-12 23:37:00 +0100

    Add get_branch_vcs_type to rust

--- crates/common-py/src/lib.rs
+++ crates/common-py/src/lib.rs
@@ -2,6 +2,13 @@ use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
 #[pyfunction]
+fn get_branch_vcs_type(branch: PyObject) -> PyResult<String> {
+    let branch = breezyshim::branch::RegularBranch::new(branch);
+    janitor::vcs::get_branch_vcs_type(&branch)
+        .map_err(|e| PyValueError::new_err((format!("{}", e),)))
+}
+
+#[pyfunction]
 fn is_authenticated_url(url: &str) -> PyResult<bool> {
     Ok(janitor::vcs::is_authenticated_url(
         &url::Url::parse(url)
@@ -21,5 +28,6 @@ pub fn _common(m: &Bound<PyModule>) -> PyResult<()> {
     pyo3_log::init();
     m.add_function(wrap_pyfunction!(is_authenticated_url, m)?)?;
     m.add_function(wrap_pyfunction!(is_alioth_url, m)?)?;
+    m.add_function(wrap_pyfunction!(get_branch_vcs_type, m)?)?;
     Ok(())
 }
