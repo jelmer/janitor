4403874594f4dd359441b26b538bf8865b68c851
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-08 14:24:05 +0100

    Add get_debdiff

--- crates/publish/src/lib.rs
+++ crates/publish/src/lib.rs
@@ -1,4 +1,7 @@
 use chrono::{DateTime, Utc};
+use reqwest::header::HeaderMap;
+
+//pub mod publish_one;
 
 pub fn calculate_next_try_time(finish_time: DateTime<Utc>, attempt_count: usize) -> DateTime<Utc> {
     if attempt_count == 0 {
@@ -7,6 +10,60 @@ pub fn calculate_next_try_time(finish_time: DateTime<Utc>, attempt_count: usize)
         let delta = chrono::Duration::hours(2usize.pow(attempt_count as u32).min(7 * 24) as i64);
 
         finish_time + delta
+    }
+}
+
+#[derive(Debug)]
+pub enum DebdiffError {
+    Http(reqwest::Error),
+    MissingRun(String),
+    Unavailable(String),
+}
+
+impl From<reqwest::Error> for DebdiffError {
+    fn from(e: reqwest::Error) -> Self {
+        DebdiffError::Http(e)
+    }
+}
+
+impl std::fmt::Display for DebdiffError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            DebdiffError::Http(e) => write!(f, "HTTP error: {}", e),
+            DebdiffError::MissingRun(e) => write!(f, "Missing run: {}", e),
+            DebdiffError::Unavailable(e) => write!(f, "Unavailable: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for DebdiffError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            DebdiffError::Http(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+pub async fn get_debdiff(differ_url: &url::Url, unchanged_id: &str, log_id: &str) -> Result<Vec<u8>, DebdiffError> {
+    let debdiff_url = differ_url.join(&format!("/debdiff/{}/{}?filter_boring=1", unchanged_id, log_id)).unwrap();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Accept", "text/plain".parse().unwrap());
+
+    let client = reqwest::Client::new();
+    let response = client.get(debdiff_url).headers(headers).send().await?;
+
+    match response.status() {
+        reqwest::StatusCode::OK => Ok(response.bytes().await?.to_vec()),
+        reqwest::StatusCode::NOT_FOUND => {
+            let run_id = response.headers().get("unavailable_run_id").unwrap().to_str().unwrap();
+            Err(DebdiffError::MissingRun(run_id.to_string()))
+        },
+        reqwest::StatusCode::BAD_REQUEST | reqwest::StatusCode::INTERNAL_SERVER_ERROR | reqwest::StatusCode::BAD_GATEWAY | reqwest::StatusCode::SERVICE_UNAVAILABLE | reqwest::StatusCode::GATEWAY_TIMEOUT => {
+            Err(DebdiffError::Unavailable(response.text().await.unwrap()))
+        },
+        _e => Err(DebdiffError::Http(response.error_for_status().unwrap_err()))
     }
 }
 
