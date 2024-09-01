0bd355346f274aa18ab7cc2587f74b0c9fbcf289
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-01 11:52:35 +0000

    Move crates to top-level

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools_rust import Binding, RustBin, RustExtension
 
 setup(
         rust_extensions=[
-            RustExtension('janitor._worker', 'crates/worker-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._common', 'crates/common-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._publish', 'crates/publish-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._runner', 'crates/runner-py/Cargo.toml', binding=Binding.PyO3),
-            RustBin('janitor-mail-filter', 'crates/mail-filter/Cargo.toml', features=['cmdline']),
+            RustExtension('janitor._worker', 'worker-py/Cargo.toml', binding=Binding.PyO3),
+            RustExtension('janitor._common', 'common-py/Cargo.toml', binding=Binding.PyO3),
+            RustExtension('janitor._publish', 'publish-py/Cargo.toml', binding=Binding.PyO3),
+            RustExtension('janitor._runner', 'runner-py/Cargo.toml', binding=Binding.PyO3),
+            RustBin('janitor-mail-filter', 'mail-filter/Cargo.toml', features=['cmdline']),
         ])
