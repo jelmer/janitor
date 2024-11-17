bbcaab91881c2cd9df9c7a7d6df974e5a1c8335f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-11-17 01:14:10 +0000

    Handle diffoscope in differ

--- differ/src/diffoscope.rs
+++ differ/src/diffoscope.rs
@@ -2,7 +2,7 @@ use patchkit::unified::{iter_hunks, Hunk, HunkLine};
 use std::path::PathBuf;
 use tracing::{debug, warn};
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 #[allow(unused)]
 pub struct DiffoscopeOutput {
     #[serde(
@@ -10,13 +10,30 @@ pub struct DiffoscopeOutput {
         skip_serializing_if = "Option::is_none"
     )]
     diffoscope_json_version: Option<u8>,
-    source1: PathBuf,
-    source2: PathBuf,
+    pub source1: PathBuf,
+    pub source2: PathBuf,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    comments: Vec<String>,
+    pub comments: Vec<String>,
     #[serde(default)]
-    unified_diff: Option<String>,
-    details: Vec<DiffoscopeOutput>,
+    pub unified_diff: Option<String>,
+    pub details: Vec<DiffoscopeOutput>,
+}
+
+impl pyo3::ToPyObject for DiffoscopeOutput {
+    fn to_object(&self, py: pyo3::Python) -> pyo3::PyObject {
+        use pyo3::prelude::*;
+        let dict = pyo3::types::PyDict::new_bound(py);
+        dict.set_item("diffoscope_json_version", self.diffoscope_json_version)
+            .unwrap();
+        dict.set_item("source1", self.source1.to_str().unwrap())
+            .unwrap();
+        dict.set_item("source2", self.source2.to_str().unwrap())
+            .unwrap();
+        dict.set_item("comments", &self.comments).unwrap();
+        dict.set_item("unified_diff", &self.unified_diff).unwrap();
+        dict.set_item("details", &self.details).unwrap();
+        dict.into()
+    }
 }
 
 #[derive(Debug)]
@@ -56,11 +73,23 @@ impl std::fmt::Display for DiffoscopeError {
     }
 }
 
+fn _set_limits(limit_mb: Option<u64>) {
+    let limit_mb = limit_mb.unwrap_or(1024);
+
+    nix::sys::resource::setrlimit(
+        nix::sys::resource::Resource::RLIMIT_AS,
+        limit_mb * 1024 * 1024,
+        limit_mb * 1024 * 1024,
+    )
+    .unwrap();
+}
+
 async fn _run_diffoscope(
     old_binary: &str,
     new_binary: &str,
     diffoscope_command: Option<&str>,
     timeout: Option<f64>,
+    memory_limit: Option<usize>,
 ) -> Result<Option<DiffoscopeOutput>, DiffoscopeError> {
     let diffoscope_command = diffoscope_command.unwrap_or("diffoscope");
     let mut args = shlex::split(diffoscope_command).unwrap();
@@ -77,6 +106,15 @@ async fn _run_diffoscope(
     cmd.stdin(std::process::Stdio::null());
     cmd.stdout(std::process::Stdio::piped());
     cmd.stderr(std::process::Stdio::piped());
+
+    if let Some(memory_limit) = memory_limit {
+        unsafe {
+            cmd.pre_exec(move || {
+                _set_limits(Some(memory_limit as u64));
+                Ok(())
+            })
+        };
+    }
 
     let output = tokio::time::timeout(
         std::time::Duration::from_secs_f64(timeout.unwrap_or(5.0)),
@@ -206,10 +244,85 @@ pub fn filter_boring(
     diff.details = new_details.collect();
 }
 
+pub fn format_diffoscope(
+    diff: &DiffoscopeOutput,
+    content_type: &str,
+    title: &str,
+    css_url: Option<&str>,
+) -> String {
+    use pyo3::prelude::*;
+    if content_type == "application/json" {
+        return serde_json::to_string(diff).unwrap();
+    }
+
+    Python::with_gil(|py| {
+        let m = py.import_bound("diffoscope.readers.json")?;
+        let reader = m.getattr("JSONReaderV1")?.call0()?;
+
+        let root_differ = reader.call_method1("load_rec", (diff.to_object(py),))?;
+
+        match content_type {
+            "text/html" => {
+                let m = py.import_bound("diffoscope.presenters.html")?;
+                let p = m.getattr("HTMLPresenter")?.call0()?;
+
+                let sysm = py.import_bound("sys")?;
+
+                let old_stdout = sysm.getattr("stdout")?;
+                sysm.setattr("stdout", p.getattr("stdout")?)?;
+                let io = py.import_bound("io")?;
+                let f = io.getattr("StringIO")?.call0()?;
+                sysm.setattr("stdout", f.clone())?;
+                let old_argv = sysm.getattr("argv")?;
+                sysm.setattr(
+                    "argv",
+                    title.split(' ').map(|s| s.into()).collect::<Vec<String>>(),
+                )?;
+
+                let kwargs = pyo3::types::PyDict::new_bound(py);
+                kwargs.set_item("css_url", css_url)?;
+                p.call_method("output_html", ("-", root_differ), Some(&kwargs))?;
+                let html = f.call_method0("getvalue")?;
+
+                sysm.setattr("stdout", old_stdout)?;
+                sysm.setattr("argv", old_argv)?;
+
+                Ok(html.extract::<String>()?)
+            }
+            "text/markdown" => {
+                let m = py.import_bound("diffoscope.presenters.markdown")?;
+                let out = pyo3::types::PyList::empty_bound(py);
+
+                let presenter = m
+                    .getattr("MarkdownPresenter")?
+                    .call1((out.getattr("append")?,))?;
+                let markdown = presenter.call_method1("start", (root_differ,))?;
+                Ok(markdown.extract::<String>()?)
+            }
+            "text/plain" => {
+                let m = py.import_bound("diffoscope.presenters.text")?;
+                let out = pyo3::types::PyString::new_bound(py, "");
+
+                let presenter = m
+                    .getattr("TextPresenter")?
+                    .call1((out.getattr("append")?, false))?;
+                presenter.call_method1("start", (root_differ,))?;
+
+                Ok(out.extract::<Vec<String>>()?.join("\n"))
+            }
+            _ => Err(pyo3::exceptions::PyValueError::new_err(
+                "Invalid content type",
+            )),
+        }
+    })
+    .unwrap()
+}
+
 pub async fn run_diffoscope(
-    old_binaries: &[(String, String)],
-    new_binaries: &[(String, String)],
+    old_binaries: &[(&str, &str)],
+    new_binaries: &[(&str, &str)],
     timeout: Option<f64>,
+    memory_limit: Option<u64>,
     diffoscope_command: Option<&str>,
 ) -> Result<DiffoscopeOutput, DiffoscopeError> {
     let mut ret = DiffoscopeOutput {
@@ -223,7 +336,14 @@ pub async fn run_diffoscope(
 
     for ((old_name, old_path), (new_name, new_path)) in old_binaries.iter().zip(new_binaries.iter())
     {
-        let sub = _run_diffoscope(old_path, new_path, diffoscope_command, timeout).await?;
+        let sub = _run_diffoscope(
+            old_path,
+            new_path,
+            diffoscope_command,
+            timeout,
+            memory_limit.map(|mb| mb as usize),
+        )
+        .await?;
         if let Some(mut sub) = sub {
             sub.source1 = old_name.into();
             sub.source2 = new_name.into();
--- differ/src/lib.rs
+++ differ/src/lib.rs
@@ -11,8 +11,6 @@ pub fn find_binaries(path: &Path) -> impl Iterator<Item = (OsString, PathBuf)> {
     })
 }
 
-pub fn is_binary(name: &OsStr) -> bool {
-    name.to_str().map_or(false, |name| {
-        name.ends_with(".deb") || name.ends_with(".udeb")
-    })
+pub fn is_binary(name: &str) -> bool {
+    name.ends_with(".deb") || name.ends_with(".udeb")
 }
