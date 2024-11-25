3b2847d3c09d80235ee8d9c4ab5dda044c9af194
Jelmer Vernooij <jelmer@jelmer.uk>
2024-11-25 21:36:02 +0000

    yaml: fix formatting

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 from setuptools_rust import Binding, RustBin, RustExtension
 
 setup(
-        rust_extensions=[
-            RustExtension('janitor._common', 'common-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._publish', 'publish-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._runner', 'runner-py/Cargo.toml', binding=Binding.PyO3),
-            RustExtension('janitor._site', 'site-py/Cargo.toml', binding=Binding.PyO3),
-            RustBin('janitor-mail-filter', 'mail-filter/Cargo.toml', features=['cmdline']),
-            RustBin('janitor-worker', 'worker/Cargo.toml', features=['cli', 'debian']),
-            RustBin('janitor-dist', 'worker/Cargo.toml', features=['cli', 'debian'])
-        ])
+    rust_extensions=[
+        RustExtension("janitor._common", "common-py/Cargo.toml", binding=Binding.PyO3),
+        RustExtension(
+            "janitor._publish", "publish-py/Cargo.toml", binding=Binding.PyO3
+        ),
+        RustExtension("janitor._runner", "runner-py/Cargo.toml", binding=Binding.PyO3),
+        RustExtension("janitor._site", "site-py/Cargo.toml", binding=Binding.PyO3),
+        RustBin("janitor-mail-filter", "mail-filter/Cargo.toml", features=["cmdline"]),
+        RustBin("janitor-worker", "worker/Cargo.toml", features=["cli", "debian"]),
+        RustBin("janitor-dist", "worker/Cargo.toml", features=["cli", "debian"]),
+    ]
+)
