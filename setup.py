9c6836b4f99f708365c09479b9b1a52c5eed7f8a
Jelmer Vernooij <jelmer@jelmer.uk>
2025-03-05 00:50:59 +0000

    Fix tests for Python modules

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools_rust import Binding, RustBin, RustExtension
 
 setup(
     rust_extensions=[
-        RustExtension("janitor._common", "common-py/Cargo.toml", binding=Binding.PyO3),
+        RustExtension("janitor._common", "common-py/Cargo.toml", binding=Binding.PyO3, features=["extension-module"]),
         RustExtension(
-            "janitor._publish", "publish-py/Cargo.toml", binding=Binding.PyO3
+            "janitor._publish", "publish-py/Cargo.toml", binding=Binding.PyO3, features=["extension-module"]
         ),
-        RustExtension("janitor._runner", "runner-py/Cargo.toml", binding=Binding.PyO3),
-        RustExtension("janitor._site", "site-py/Cargo.toml", binding=Binding.PyO3),
+        RustExtension("janitor._runner", "runner-py/Cargo.toml", binding=Binding.PyO3, features=["extension-module"]),
+        RustExtension("janitor._site", "site-py/Cargo.toml", binding=Binding.PyO3, features=["extension-module"]),
         RustBin("janitor-mail-filter", "mail-filter/Cargo.toml", features=["cmdline"]),
         RustBin("janitor-worker", "worker/Cargo.toml", features=["cli", "debian"]),
         RustBin("janitor-dist", "worker/Cargo.toml", features=["cli", "debian"]),
