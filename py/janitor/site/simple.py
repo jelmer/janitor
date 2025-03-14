4881d82826e6ba88df731f16a3950cdd40ee338b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-03-14 14:44:19 +0000

    Update py/janitor/site/simple.py

--- py/janitor/site/simple.py
+++ py/janitor/site/simple.py
@@ -304,6 +304,8 @@ async def handle_repo_list(request):
         vcs = request.app["vcs_managers"][vcs_name]
     except KeyError:
         raise web.HTTPNotFound()
+    else:
+        url = vcs.base_url
     async with request.app["http_client_session"].get(url) as resp:
         return {"vcs": vcs, "repositories": await resp.json()}
 
