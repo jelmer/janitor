186ccb5eb575c3264a2bf03fdfe950bd71901b59
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-08-25 01:19:53 +0100

    site: test /{campaign}/c query-string redirect

--- tests/test_site_simple.py
+++ tests/test_site_simple.py
@@ -29,3 +29,46 @@ campaign {
 
 async def test_create_app():
     await create_app(config=create_config())
+
+
+async def test_codebase_query_redirect(aiohttp_client):
+    client = await aiohttp_client(await create_app(config=create_config()))
+    resp = await client.get(
+        "/lintian-fixes/c", params={"codebase": "foo"}, allow_redirects=False
+    )
+    assert resp.status == 302
+    assert resp.headers["Location"] == "/lintian-fixes/c/foo/"
+
+
+async def test_codebase_query_redirect_legacy_package_param(aiohttp_client):
+    client = await aiohttp_client(await create_app(config=create_config()))
+    resp = await client.get(
+        "/lintian-fixes/c", params={"package": "foo"}, allow_redirects=False
+    )
+    assert resp.status == 302
+    assert resp.headers["Location"] == "/lintian-fixes/c/foo/"
+
+
+async def test_codebase_query_redirect_prefers_codebase_over_package(aiohttp_client):
+    client = await aiohttp_client(await create_app(config=create_config()))
+    resp = await client.get(
+        "/lintian-fixes/c",
+        params={"codebase": "new", "package": "old"},
+        allow_redirects=False,
+    )
+    assert resp.status == 302
+    assert resp.headers["Location"] == "/lintian-fixes/c/new/"
+
+
+async def test_codebase_query_redirect_missing_param_returns_404(aiohttp_client):
+    client = await aiohttp_client(await create_app(config=create_config()))
+    resp = await client.get("/lintian-fixes/c", allow_redirects=False)
+    assert resp.status == 404
+
+
+async def test_codebase_query_redirect_empty_param_returns_404(aiohttp_client):
+    client = await aiohttp_client(await create_app(config=create_config()))
+    resp = await client.get(
+        "/lintian-fixes/c", params={"codebase": ""}, allow_redirects=False
+    )
+    assert resp.status == 404
